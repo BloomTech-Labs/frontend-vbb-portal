2ab48bf5a8300727f8f8530d7df148462b0de8a7
Brigham Andersen <brighamband@gmail.com>
2020-07-24 12:15:39 -0600

    added booking

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
+import Booking from "./components/Booking";
 import Profile from "./components/Profile";
 
 const BaseRouter = () => (
   <div>
     <Route exact path="/" component={Profile} />
+    <Route exact path="/booking/" component={Booking} />
     {/* <Route exact path="/profile/" component={Profile} /> */}
   </div>
 );
