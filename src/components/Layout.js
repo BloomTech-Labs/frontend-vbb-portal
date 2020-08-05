73d2c153013c241a5af51eb0edbca9750bfe6302
Brigham Andersen <brighamband@gmail.com>
2020-08-05 13:06:44 -0600

    Finished navbar fixes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,64 +3,96 @@ import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "../store/actions/auth";
 import fullLogo from "../vbb-full-logo.png";
+import miniLogo from "../vbb-picture-logo.png";
 
 class Layout extends React.Component {
   render() {
     return (
       <div className="layout">
-        <nav className="navbar sticky-top navbar-dark">
+        <nav
+          className="navbar sticky-top  navbar-expand-lg navbar-light"
+          id="vbb-full-bar"
+        >
           <a
+            id="full-logo"
             href="https://www.villagebookbuilders.org/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={fullLogo} alt="Logo" style={{ width: "250px" }} />
+            <img src={fullLogo} alt="Logo" style={{ width: "200px" }} />
           </a>
-          <a href="/">
-            <h1 style={{ position: "relative", top: "20px" }}>VBB Portal</h1>
+          <a
+            id="mini-logo"
+            href="https://www.villagebookbuilders.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={miniLogo}
+              alt="Logo"
+              style={{ width: "45px", marginLeft: "-10px" }}
+            />
           </a>
-          {this.props.isAuthenticated ? (
-            <div>
-              <a
-                className="btn btn-light donate-btn"
-                type="button"
-                href="https://www.villagebookbuilders.org/giftabook/"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ position: "relative", top: "15px" }}
-              >
-                DONATE
-              </a>
-              <a
-                className="btn btn-light signin-btn"
-                type="button"
-                href="/signin/"
-                style={{ position: "relative", top: "15px" }}
-                onClick={this.props.logout}
-              >
-                SIGN OUT
-              </a>
-            </div>
-          ) : (
-            <div>
-              <a
-                className="btn btn-light donate-btn"
-                type="button"
-                href="/signup/"
-                style={{ position: "relative", top: "15px" }}
-              >
-                REGISTER
-              </a>
-              <a
-                className="btn btn-light signin-btn"
-                type="button"
-                href="/signin/"
-                style={{ position: "relative", top: "15px" }}
-              >
-                SIGN IN
-              </a>
-            </div>
-          )}
+          <h1
+            id="bar-header"
+            style={{ marginBottom: "-25px", fontSize: "40px" }}
+          >
+            VBB Portal
+          </h1>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            {this.props.isAuthenticated ? (
+              <div className="btn-pair">
+                <a
+                  className="btn btn-light donate-btn"
+                  type="button"
+                  href="https://www.villagebookbuilders.org/giftabook/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ marginRight: "20px" }}
+                >
+                  DONATE
+                </a>
+                <a
+                  className="btn btn-light signout-btn"
+                  type="button"
+                  href="/signin/"
+                  onClick={this.props.logout}
+                >
+                  SIGN OUT
+                </a>
+              </div>
+            ) : (
+              <div className="btn-pair">
+                <a
+                  className="btn btn-light signup-btn"
+                  type="button"
+                  href="/signup/"
+                  style={{ marginRight: "20px" }}
+                >
+                  REGISTER
+                </a>
+                <a
+                  className="btn btn-light signin-btn"
+                  type="button"
+                  href="/signin/"
+                >
+                  SIGN IN
+                </a>
+              </div>
+            )}
+          </div>
         </nav>
 
         <div className="content">{this.props.children}</div>
