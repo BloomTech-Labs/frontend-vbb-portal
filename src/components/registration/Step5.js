ac0880fa850b29096b11a7ea76ec0c4a3f517b0b
Brigham Andersen <brighamband@gmail.com>
2020-08-11 14:19:30 -0600

    Fixed weird formatting on step4

--- src/components/registration/Step5.js
+++ src/components/registration/Step5.js
@@ -56,6 +56,9 @@ function Step5(props) {
               <option value="research"> Research</option>
               <option value="other"> Other</option>
             </select>
+            <br />
+            <br />
+            <br />
           </div>
         )}
 
