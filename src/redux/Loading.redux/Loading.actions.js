f46aeeecd4901cafc7b827ff299c48962f0ae277
wley3337 <wley3337@gmail.com>
2021-01-04 19:02:45 -0500

    Update Loading action types and comments

--- src/redux/Loading.redux/Loading.actions.js
+++ src/redux/Loading.redux/Loading.actions.js
@@ -1,6 +1,13 @@
+/**
+ * Sets the Loading state of the app to true
+ */
 export const setLoading = () => {
-  return { type: 'SET_LOADING' };
+  return { type: 'SET_LOADING_TRUE' };
 };
+
+/**
+ * Sets the Loading state of the app to false
+ */
 export const setFinishLoading = () => {
-  return { type: 'SET_FINISH_LOADING' };
+  return { type: 'SET_LOADING_FALSE' };
 };
--- src/redux/Loading.redux/Loading.reducer.js
+++ src/redux/Loading.redux/Loading.reducer.js
@@ -3,12 +3,15 @@ const loadingInitialState = false;
 /*
  * Sets the loading state of the App
  *
+ * Action :  {
+ * type: 'SET_LOADING_TRUE', 'SET_LOADING_FALSE'
+ * }
  */
 export const loading = (state = loadingInitialState, action) => {
   switch (action.type) {
-    case 'SET_LOADING':
+    case 'SET_LOADING_TRUE':
       return true;
-    case 'SET_FINISH_LOADING':
+    case 'SET_LOADING_FALSE':
       return false;
     default:
       return state;
