e9b4f4f16eba082f35641204919e19d4076ec490
wley3337 <wley3337@gmail.com>
2021-01-04 19:00:50 -0500

    Update comments and standardize action.payload vs action.X

--- src/redux/IsError.redux/IsError.reducer.js
+++ src/redux/IsError.redux/IsError.reducer.js
@@ -3,17 +3,17 @@ const isErrorInitialState = {
   isError: false,
 };
 
-/*
- * Sets the IsError state of the App
+/**
+ * Reducer for the IsError state of the App
  * Action :  {
- * type: SET_IS_ERROR, 'CLEAR_IS_ERROR'
- * message: string
+ * type: 'SET_IS_ERROR', 'CLEAR_IS_ERROR'
+ * payload: string Error message to be added to store
  * }
  */
 export const isError = (state = isErrorInitialState, action) => {
   switch (action.type) {
     case 'SET_IS_ERROR':
-      return { isError: true, message: action.message ?? 'No error message returned' };
+      return { isError: true, message: action.payload ?? 'No error message returned' };
     case 'CLEAR_IS_ERROR':
       return initialState;
     default:
