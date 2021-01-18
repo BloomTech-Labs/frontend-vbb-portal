2205c3c038e6e0ad3f9c5514d68578383c26faaf
wley3337 <wley3337@gmail.com>
2021-01-18 13:06:12 -0500

    Add BASE_URL const for switching of backend pointers

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -2,6 +2,9 @@
  * base level export for all actions in the application
  */
 
+//@TODO temporary, this should be an ENV var
+export const BASE_URL = 'http://127.0.0.1:8000';
+
 export * from '../Loading.redux/Loading.actions';
 export * from '../IsError.redux/IsError.actions';
 export * from '../Login.redux/Login.actions';
