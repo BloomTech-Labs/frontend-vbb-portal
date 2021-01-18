4a63bda29d3f92da7f509f25cfede5a1bbf599e5
wley3337 <wley3337@gmail.com>
2021-01-17 22:20:13 -0500

    Convert to functional component, extract redux action functions, add notes on refactor

--- src/components/HomeSignin.js
+++ src/components/HomeSignin.js
@@ -1,76 +1,87 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { GoogleLogin } from 'react-google-login';
-
-// import * as actions from '../store/actions/auth';
+import { Link } from 'react-router-dom';
 import * as actions from '../redux/actions';
 import vbbInAction from '../images/vbb-in-action.png';
 
-export class HomeSignin extends Component {
-  responseGoogle = (response) => {
-    console.log('failure response', response);
-    alert(
-      'Google authorization failed. Please Make sure Cookies are enabled on your browser! ... ',
-      response
-    );
-  };
-  validateGoogle = (response) => {
-    this.props.onGauth(response.accessToken);
-    this.props.history.push('/');
-  };
+// this needs  to hold the goolge sign in button
+// registration button should be a link to registration route from react-router-dom
+// this should all be moved to actions
+const responseGoogle = (response) => {
+  console.log('failure response', response);
+  alert(
+    'Google authorization failed. Please Make sure Cookies are enabled on your browser! ... ',
+    response
+  );
+};
+//   // responseGoogle = (response) => {
+//   //   console.log('failure response', response);
+//   //   alert(
+//   //     'Google authorization failed. Please Make sure Cookies are enabled on your browser! ... ',
+//   //     response
+//   //   );
+//   // };
+const validateGoogle = (response, history, onGauth) => {
+  onGauth(response.accessToken);
+  history.push('/');
+};
 
-  render() {
-    return (
-      <div className="twocol-container">
-        <div className="column" id="signin-box">
-          <h1 id="welcome-header">Welcome to VBB Portal!</h1>
-          <h4 id="portal-purpose">
-            Let's give hope to children through mentoring.
-          </h4>
-          <br />
-          <div id="google-box">
-            <GoogleLogin
-              clientId="711431548719-lpoc2lbr4bmruqo7d9emua5huvpsvqgk.apps.googleusercontent.com"
-              buttonText="Click here to sign in!"
-              onSuccess={this.validateGoogle}
-              onFailure={this.responseGoogle}
-              cookiePolicy={'single_host_origin'}
-              style={{ width: '100%', paddingTop: '30px' }}
-            />
-          </div>
-          <br />
-          <hr id="sep-bar" />
-          <br />
-          <div id="create-account-box">
-            <p
-              style={{
-                paddingLeft: '0px',
-                fontSize: '20px',
-                color: '#ff914d',
-                fontWeight: 'bolder',
-              }}
-            >
-              Don't have an account with us yet?
-            </p>
-            <a
-              className="btn btn-light signup-btn"
-              type="button"
-              href="/signup/"
-            >
-              REGISTER
-            </a>
-          </div>
+/**
+ * HomeSignin.
+ * Connected functional component
+ * Displays Google Signin button and Register button
+ *
+ * @param { Type History } history from react router dom
+ * @param {onGauth} current action from login import as a connected component
+ */
+const HomeSignin = ({ history, onGauth }) => {
+  return (
+    <div className="twocol-container">
+      <div className="column" id="signin-box">
+        <h1 id="welcome-header">Welcome to VBB Portal!</h1>
+        <h4 id="portal-purpose">
+          Let's give hope to children through mentoring.
+        </h4>
+        <br />
+        <div id="google-box">
+          <GoogleLogin
+            clientId="711431548719-lpoc2lbr4bmruqo7d9emua5huvpsvqgk.apps.googleusercontent.com"
+            buttonText="Click here to sign in!"
+            onSuccess={(res) => validateGoogle(res, history, onGauth)}
+            onFailure={(res) => responseGoogle(res)}
+            cookiePolicy={'single_host_origin'}
+            style={{ width: '100%', paddingTop: '30px' }}
+          />
         </div>
-        <img
-          src={vbbInAction}
-          id="signin-picture"
-          alt="Pic"
-          style={{ width: '600px', margin: '5vw' }}
-        />
+        <br />
+        <hr id="sep-bar" />
+        <br />
+        <div id="create-account-box">
+          <p
+            style={{
+              paddingLeft: '0px',
+              fontSize: '20px',
+              color: '#ff914d',
+              fontWeight: 'bolder',
+            }}
+          >
+            Don't have an account with us yet?
+          </p>
+          <Link to="/signup/" className="btn btn-light signup-btn">
+            REGISTER
+          </Link>
+        </div>
       </div>
-    );
-  }
-}
+      <img
+        src={vbbInAction}
+        id="signin-picture"
+        alt="Two mentors reading to two mentees on the steps of a Village Book Builders' library"
+        style={{ width: '600px', margin: '5vw' }}
+      />
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
@@ -79,10 +90,16 @@ const mapStateToProps = (state) => {
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onGauth: (googleToken) => dispatch(actions.gAuth(googleToken)),
-  };
-};
+// need to sort out google auth
+// this should make a call to goolge
+// get token back
+// send token to backend
+// receive cookie auth jwt that's stored and send as part of a header
 
-export default connect(mapStateToProps, mapDispatchToProps)(HomeSignin);
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     onGauth: (googleToken) => dispatch(actions.gAuth(googleToken)),
+//   };
+// };
+
+export default connect(mapStateToProps, actions)(HomeSignin);
