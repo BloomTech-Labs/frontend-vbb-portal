152a51db71c03461d52d5229c5aad10536156bf8
wley3337 <wley3337@gmail.com>
2021-03-03 19:29:00 -0500

    Move routes to containers folder

---
+++ src/containers/Routes.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+import PrivateRoute from '../components/PrivateRoute';
+import Booking from '../components/Booking';
+import Dashboard from '../components/Dashboard';
+import MasterForm from '../components/registration/MasterForm';
+import HomeSignin from '../components/HomeSignin';
+import SessionDetails from '../components/SessionDetails';
+// import Donation from "./registration/Donation"
+
+const Routes = () => {
+  return (
+    <div>
+      <PrivateRoute exact path="/" component={Dashboard} />
+      <PrivateRoute exact path="/booking/" component={Booking} />
+      <Route exact path="/signin/" component={HomeSignin} />
+      <Route exact path="/signup/" component={MasterForm} />
+      <Route
+        exact
+        path="/sessiondetails/:sessionid/"
+        component={SessionDetails}
+      />
+      {/* <Route exact path="/donate/" component={Donation} /> */}
+    </div>
+  );
+};
+
+export default Routes;
