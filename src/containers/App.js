0ca177959692b1177cdb84396243d663c3195240
wley3337 <wley3337@gmail.com>
2021-03-03 20:46:05 -0500

    Move Layout to Containers

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 
 import '../style.css';
 import * as actions from '../redux/actions';
-import Layout from '../components/Layout';
+import Layout from './Layout';
 import Routes from './Routes';
 
 class App extends Component {
