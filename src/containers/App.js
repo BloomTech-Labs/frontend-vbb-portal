ecb2fcaf0c688e645fefc54345f0cdc954439a39
wley3337 <wley3337@gmail.com>
2021-03-08 11:26:22 -0500

    Update app component to be functional

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,37 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-
 import '../style.css';
-import * as actions from '../redux/actions';
+
 import Layout from './Layout';
-import Routes from './Routes';
 
-class App extends Component {
-  componentDidMount() {
-    this.props.authCheckState();
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <BrowserRouter {...this.props}>
-          <Layout {...this.props}>
-            <Routes />
-          </Layout>
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    isAuthenticated: state.token !== null,
-    isLoading: state.loading,
-    error: state.isError.isError,
-    message: state.isError.message,
-  };
+const App = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </div>
+  );
 };
 
-export default connect(mapStateToProps, actions)(App);
+export default App;
