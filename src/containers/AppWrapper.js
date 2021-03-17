1f3a6c061fc63f9290fd188d5b20695acabffac9
Mary Baker <meb601@gmail.com>
2021-03-17 10:50:55 -0400

    Comment out pseudocode for nav button ternary.

--- src/containers/AppWrapper.js
+++ src/containers/AppWrapper.js
@@ -3,10 +3,26 @@
 import React from 'react';
 import { Layout, Button, PageHeader, Breadcrumb } from 'antd';
 import { LoginOutlined, FormOutlined } from '@ant-design/icons';
+import * as actions from '../redux/actions';
 import fullLogo from '../images/vbb-full-logo.png';
+import { logOut } from '../redux/actions';
 
 function AppWrapper(props) {
-    const { Footer, Content } = Layout;
+    // const { Footer, Content } = Layout;
+
+    // const handleOnClick = () => {
+    //     logOut()
+    // }
+
+    // const buttons = (urlPath) => {
+    //     if(urlPath.includes('/signin')) {
+    //             <Button key="1" href='/donate/' style={{ marginTop: '15px', color: '#549bea' }}>Donate<LoginOutlined /></Button>,
+    //             <Button type='primary' key="2" onClick={ handleOnClick }>Logout<FormOutlined style={{ color: 'white'}}/></Button>
+    //     } else {
+    //             <Button key="1" href='/signin/' style={{ marginTop: '15px', color: '#549bea' }}>Sign In<LoginOutlined /></Button>,
+    //             <Button type='primary' key="2" href='/signup/'>Register<FormOutlined style={{ color: 'white'}}/></Button>
+    //     }
+    // }
 
     return (
         <div>
@@ -18,10 +34,8 @@ function AppWrapper(props) {
                         alt="VBB logo"
                         width="200"
                     ></img>}
-                    extra={[
-                        <Button key="1" href='/signin/' style={{ marginTop: '15px', color: '#549bea' }}>Sign In<LoginOutlined /></Button>,
-                        <Button type='primary' key="2" href='/signup'>Register<FormOutlined style={{ color: 'white'}}/></Button>,
-                    ]}
+                    //add a ternary if registering display below two buttons, if not registering, display donate and signout buttons
+                    extra={[{ buttons }]}
                 >
                 </PageHeader>
                 <Content className="site-layout" style={{ padding: '50px', marginTop: 64, backgroundColor: '#fffff6' }}>
