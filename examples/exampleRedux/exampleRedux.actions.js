50457e20de06d8a8b27b2103218f7192dbd78efd
Nick Foden <nickfoden@gmail.com>
2021-04-02 18:55:33 -0400

    Remove unused deps update others to current v's clear lint warnings (#49)
    * Remove unused deps update others to current v's
    
    * rename example reducer clean up lint warnings
    
    * add helper
    
    * Export the example redux action

--- examples/exampleRedux/exampleRedux.actions.js
+++ examples/exampleRedux/exampleRedux.actions.js
@@ -1,6 +1,6 @@
 const ACTION_TYPE = 'actionType';
 
-//action exported and then imported to and exported from `src/redux/actions/index.js`
-const actionVerbUsedToDescribeAction = (payload) => {
+// action exported and then imported to and exported from `src/redux/actions/index.js`
+export const actionVerbUsedToDescribeAction = (payload) => {
   return { type: ACTION_TYPE, payload: payload };
 };
