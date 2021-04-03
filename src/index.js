fbc16ca526539791b6fcad259d866089891e825f
Nick Foden <nickfoden@gmail.com>
2021-04-03 10:04:47 -0400

    deregister service worker for now (#61)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import App from './containers/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-registerServiceWorker();
+/** FUTURE: Put back when in production / after launch
+ * registering this can lead to dev seeing old versions of platform
+ * https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API
+ */
+
+// registerServiceWorker();
