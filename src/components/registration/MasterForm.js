f3c8daec36f9fef549a1fce0aac082ff00d31f60
wley3337 <wley3337@gmail.com>
2021-04-04 12:12:21 -0400

    Prittier file add submit step on first registration form

--- src/components/registration/MasterForm.js
+++ src/components/registration/MasterForm.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
+import { connect } from 'redux';
+import * as actions from '../../redux/actions';
 import { Button, Form } from 'antd';
 import { RightOutlined, LeftOutlined, CheckOutlined } from '@ant-design/icons';
+
 import Step1 from './Step1';
 import Step2 from './Step2';
 import Step3 from './Step3';
 import Step4 from './Step4';
 import ProgressBar from './ProgressBar';
 
-const MasterForm = () => {
+const MasterForm = ({ subInitialUserRegistration }) => {
   let [currentStep, setCurrentStep] = useState(1);
 
   const next = () => {
@@ -24,76 +27,72 @@ const MasterForm = () => {
   const backButton = () => {
     if (currentStep == 2 || currentStep == 3) {
       return (
-        <Button
-          style={{ marginRight: '10px'}}
-          type="button"
-          onClick={back}
-        >
+        <Button style={{ marginRight: '10px' }} type="button" onClick={back}>
           <LeftOutlined />
           Back
         </Button>
       );
     }
     return null;
-  }
+  };
 
   const nextButton = () => {
-    if (currentStep == 1 || currentStep == 2) {
-      return (
-        <Button
-          style={{ marginRight: '10px'}}
-          type="button"
-          onClick={next}
-        >
-          Next
-          <RightOutlined />
-        </Button>
-      );
+    switch (currentStep) {
+      case 1:
+        return (
+          <Button
+            style={{ marginRight: '10px' }}
+            type="button"
+            onClick={() => {
+              //@todo: how do we handle previously submitted user?
+              subInitialUserRegistration();
+              next();
+            }}
+          >
+            Next
+            <RightOutlined />
+          </Button>
+        );
+
+      case 2:
+        return (
+          <Button style={{ marginRight: '10px' }} type="button" onClick={next}>
+            Next
+            <RightOutlined />
+          </Button>
+        );
+      default:
+        return null;
     }
-    return null;
-  }
+  };
 
   const registerButton = () => {
     if (currentStep === 3) {
       return (
-        <Button
-          style={{ marginRight: '10px'}}
-          type="button"
-          onClick={next}
-        >
+        <Button style={{ marginRight: '10px' }} type="button" onClick={next}>
           Register
           <CheckOutlined />
         </Button>
       );
     }
     return null;
-  }
+  };
 
   return (
     <div>
       {currentStep < 4 ? (
         <div style={{ margin: '0 0 25px 0' }}>
-          <ProgressBar
-            currentStep={currentStep}
-          />
-      </div>
+          <ProgressBar currentStep={currentStep} />
+        </div>
       ) : (
         <div></div>
       )}
 
       <Form>
-        <Step1
-          currentStep={currentStep}
-        />
-        <Step2
-          currentStep={currentStep}
-        />
-        <Step3
-          currentStep={currentStep}
-        />
-        <Step4
-          currentStep={currentStep}
-        />
+        <Step1 currentStep={currentStep} />
+        <Step2 currentStep={currentStep} />
+        <Step3 currentStep={currentStep} />
+        <Step4 currentStep={currentStep} />
         {backButton()}
         {nextButton()}
         {registerButton()}
@@ -102,4 +101,4 @@ const MasterForm = () => {
   );
 };
 
-export default MasterForm;
+export default connect(null, actions)(MasterForm);
