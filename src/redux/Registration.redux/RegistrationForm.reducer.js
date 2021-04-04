fc27be69c8c3a14d637925f879762e1ba21c3609
wley3337 <wley3337@gmail.com>
2021-04-04 16:57:18 -0400

    Add additional fields to reg form obj

--- src/redux/Registration.redux/RegistrationForm.reducer.js
+++ src/redux/Registration.redux/RegistrationForm.reducer.js
@@ -6,6 +6,27 @@ const registrationFormInitialState = {
   email: '',
   phone: '',
   subToNewsLetter: true,
+  is_adult: false,
+  timeZone: '',
+  address: '', // Washington, DC
+  desiredInvolvement: '',
+  affiliation: '',
+  additionalInformation: {
+    workStatus: '',
+    referralSource: '',
+    language: {
+      english: false,
+      spanish: false,
+      french: false,
+      german: false,
+    },
+    convicted: false,
+    moreThanFourMonths: false,
+    initials: '',
+    getMoreInvolved: false,
+    howMoreInvolved: [],
+    agreeTermsAndConditions: false,
+  },
 };
 
 export const registrationForm = (
