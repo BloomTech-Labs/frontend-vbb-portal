019b8600e31b67108f7e31377366d43105d49d95
Mary Baker <meb601@gmail.com>
2021-04-09 13:03:14 -0400

    Import remaining step 1 subComponents into TempRegistration.

--- src/components/registration/TempRegistration.js
+++ src/components/registration/TempRegistration.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Form, Row, Col } from 'antd';
 
+import Email from './step1SubComponents/Email';
 import FirstName from './step1SubComponents/FirstName';
+import LastName from './step1SubComponents/LastName';
+import Newsletter from './step1SubComponents/Newsletter';
+import Phone from './step1SubComponents/Phone';
 import TimeZone from './step2SubComponents/TimeZone';
 import Address from './step2SubComponents/Address';
 
@@ -12,7 +16,11 @@ function TempRegistration() {
             <Row>
                 <Col xs={24} sm={24} md={24} lg={16} xl={12}>
                     <Form name="register" layout="vertical" scrollToFirstError>
+                        <Email />
                         <FirstName />
+                        <LastName />
+                        <Newsletter />
+                        <Phone />
                         <TimeZone />
                         <Address />
                     </Form>
