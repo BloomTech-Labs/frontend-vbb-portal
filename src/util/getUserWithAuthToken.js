64e2308950739b9e9e3bb6dfbfccfa9e0649357d
wley3337 <wley3337@gmail.com>
2021-04-13 19:27:36 -0400

    Comment: Remove try/catch block and let caller handle manageing the error

--- src/util/getUserWithAuthToken.js
+++ src/util/getUserWithAuthToken.js
@@ -4,7 +4,7 @@ import { PYTHON_API } from '../redux/actions';
  * This gets the user from the API using the auth token.
  * NOTE Not an action creator
  * @param {string}authToken authToken
- * @returns null or user response from the API
+ * @returns data (user) response from api or raises an error
  */
 export const getUserFromAuthToken = async (authToken = null) => {
   // if no auth token raise error
@@ -12,25 +12,21 @@ export const getUserFromAuthToken = async (authToken = null) => {
     return new Error('authToken required to login user');
   }
 
-  try {
-    //get user from backend
-    const headers = {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${authToken}`,
-    };
-    const { data, status } = await axios.get(
-      PYTHON_API + 'v1/auth/getcurrentuser/',
-      headers
-    );
+  //get user from backend
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${authToken}`,
+  };
+  const { data, status } = await axios.get(
+    PYTHON_API + 'v1/auth/getcurrentuser/',
+    headers
+  );
 
-    // if the response is not a 200
-    if (status !== 200) {
-      throw new Error('Error getting User from Auth token');
-    }
-
-    return data;
-  } catch (err) {
+  // if the response is not a 200
+  if (status !== 200) {
     //this is left vague so that we don't return any user info accidentally
     throw new Error('Error getting User from Auth token');
   }
+
+  return data;
 };
