047069c2414ab18df2ebccbce03ef2ea8910211d
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2021-04-20 14:03:46 -0400

    Uncommented google signin

--- src/components/HomeSignin.js
+++ src/components/HomeSignin.js
@@ -7,7 +7,7 @@ import {
   EyeTwoTone,
 } from '@ant-design/icons';
 import { connect } from 'react-redux';
-// import { GoogleLogin } from 'react-google-login';
+import { GoogleLogin } from 'react-google-login';
 import * as actions from '../redux/actions';
 import { Link } from 'react-router-dom';
 
