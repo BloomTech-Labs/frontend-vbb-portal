3fe5faa1f4f7b6ea0b214dc6a222fb68de84909f
jalvaradWD <jalvarado1337@lavabit.com>
2021-04-20 21:06:29 -0500

    Implemented autocomplete functionality for the search bar.

--- src/components/search-bar/beginning.js
+++ src/components/search-bar/beginning.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React from 'react';
+import { AutoComplete } from 'antd';
+import dummy from './MOCK_DATA.json';
 
+const options = dummy.map((user) => ({
+  value: `${user.first_name} ${user.last_name}`,
+}));
 
-class SearchBar extends React.Component {
-    render(){    
-        return(
-            <div>
-            <form> 
-                <input></input>
-                <button type="submit">Submit</button>
-            </form>
-            </div>
+const SearchBarAutoComplete = () => {
+  return (
+    <AutoComplete
+      style={{ width: 500 }}
+      options={options}
+      placeholder="Search for User"
+      filterOption={(inputValue, option) =>
+        option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+      }
+    ></AutoComplete>
+  );
+};
 
-        )
-        
-}} 
-
-
-export default SearchBar;
+export default SearchBarAutoComplete;
