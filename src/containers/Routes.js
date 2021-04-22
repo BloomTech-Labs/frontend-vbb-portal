41d8bd9e375f140e4b94d69b5798b63d09a38fdf
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2021-04-22 19:46:35 -0400

    added route to searchbar

--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -9,11 +9,15 @@ import HomeSignin from '../components/HomeSignin';
 import SessionDetails from '../components/SessionDetails';
 import Donation from '../components/registration/Donation';
 import TempRegistration from '../components/registration/TempRegistration';
+import SearchBarAutoComplete  from '../components/search-bar/beginning';
 
 const Routes = () => {
   return (
     <div className="App">
       <Switch>
+        <Route exact path = "/searchbar">
+          <SearchBarAutoComplete/>
+        </Route>
         <Route exact path="/register/">
           <TempRegistration />
         </Route>
