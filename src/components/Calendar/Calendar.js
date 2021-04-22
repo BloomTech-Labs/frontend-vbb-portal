7ae9ab7c6e4ef9cd6fd08c1b0f48419e79810518
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2021-04-21 21:42:14 -0400

    Removed fullcalendar interaction dependency

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -2,17 +2,12 @@ import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
-
-
-
 
 const Calendar = () => {
 
-
     return (
         <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            plugins={[dayGridPlugin, timeGridPlugin]}
             initialView="timeGridWeek"
             headerToolbar={{
                 left: 'prev,next today',
