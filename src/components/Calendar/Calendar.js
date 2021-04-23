613dbc8632cb7d882f046eaa8c70ae75b8e5f6af
Liz Shulman <corgipower@hotmail.com>
2021-04-22 20:25:17 -0400

    added wrapper-div

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -2,25 +2,28 @@ import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
+import './calendar.css'
 
 const Calendar = () => {
     return (
-        <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin]}
-            initialView="timeGridWeek"
-            nowIndicator="true"
-            customButtons={{
-                myCustomButton: {
-                    text: "▼",
-                    // click:
-                },
-            }}
-            headerToolbar={{
-                left: 'prev,next today',
-                center: 'title',
-                right: 'dayGridMonth,timeGridWeek,timeGridDay,myCustomButton'
-              }}
-        />
+        <div className="wrapper-div">
+            <FullCalendar
+                plugins={[dayGridPlugin, timeGridPlugin]}
+                initialView="timeGridWeek"
+                nowIndicator="true"
+                customButtons={{
+                    myCustomButton: {
+                        text: "▼",
+                        // click:
+                    },
+                }}
+                headerToolbar={{
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay,myCustomButton'
+                }}
+            />
+        </div>
     )
 }
 
