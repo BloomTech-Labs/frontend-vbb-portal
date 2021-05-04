e8b132d7d7dbdfdfa58cb644e3662d93e42bb3d9
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2021-05-03 21:32:32 -0400

    added css file for modal

--- src/components/search-bar/StudentInfo.js
+++ src/components/search-bar/StudentInfo.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Modal from "./Modal.css"
+
 
 const StudentInfo = () => {
     const [student, setStudent] = React.useState({
@@ -37,8 +39,8 @@ const StudentInfo = () => {
     };
 
 return (
-    <form>
-        {/* <label>
+    <form className = "StudentInfoForm">
+        {/* <label className = "label">
             Count
             <br></br>
             <input 
@@ -50,7 +52,7 @@ return (
             </input>
         </label>
         <br></br>
-        <label>
+        <label className = "label">
             Next
             <br></br>
             <input 
@@ -61,109 +63,109 @@ return (
             >
             </input>
         </label>
-        <label>
+        <label className = "label">
            Previous
            <br></br>
             <input placeholder={student.previous}>
             </input>
         </label>
         <br></br> */}
-        {/* <label>
+        {/* <label className = "label">
             ID
             <br></br>
             <input placeholder={student.results[0].id}>
             </input>
         </label> */}
-        <label>
+        <label className = "label">
             First Name
             <br></br>
             <input placeholder={student.results[0].user.first_name}>
             </input>
         </label>
-        <label>
+        <label className = "label">
             Last Name
             <br></br>
             <input placeholder={student.results[0].user.last_name}>
             </input>
         </label>
         <br></br>
-        <label>
+        <label className = "label">
             Date of Birth
             <br></br>
             <input placeholder={student.results[0].user.date_of_birth}>
             </input>
         </label>
-        <label>
+        <label className = "label">
             Time Zone
             <br></br>
             <input placeholder={student.results[0].user.time_zone}>
             </input>
         </label>
-        <label>
+        <label className = "label">
             Initials
             <br></br>
             <input placeholder={student.results[0].user.initials}>
             </input>
         </label>
         <br></br>
-        <label>
+        <label className = "label">
             Personal Email
             <br></br>
             <input placeholder={student.results[0].user.personal_email}>
             </input>
         </label>
-        <label>
+        <label className = "label">
             Phone
             <br></br>
             <input placeholder={student.results[0].user.phone}>
             </input>
         </label>
-        <label>
+        <label className = "label">
             City
             <br></br>
             <input placeholder={student.results[0].user.city}>
             </input>
         </label>
         <br></br>
-        <label>
+        <label className = "label">
             Notes
             <br></br>
             <input placeholder={student.results[0].user.notes}>
             </input>
         </label>
-        {/* <label>
+        {/* <label className = "label">
             Created Date
             <br></br>
             <input placeholder={student.results[0].created_date}>
             </input>
         </label> */}
-        {/* <label>
+        {/* <label className = "label">
             Motified Date
             <br></br>
             <input placeholder={student.results[0].modified_date}>
             </input>
         </label>
         <br></br>
-        <label>
+        <label className = "label">
             External ID
             <br></br>
             <input placeholder={student.results[0].external_id}>
             </input>
         </label> */}
-        <label>
+        <label className = "label">
             School Level
             <br></br>
             <input placeholder={student.results[0].school_level}>
             </input>
         </label>
-        <label>
+        <label className = "label">
             Group Name
             <br></br>
             <input placeholder={student.results[0].group_name}>
             </input>
         </label>
         <br></br>
-        <button>Edit</button>
+        <button className = "button">Edit</button>
     </form>
 )
 }
