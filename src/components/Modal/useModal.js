6c123a9e2129d539fc037a29028e0eb5b05d17bf
Germaine P <me@germainep.com>
2021-05-04 20:23:16 -0400

    fix: extracted modal logic to a custom hook
    used a custom hook to extract the modal's logic. The rest of the modal is an instance of Ant Design modal component.

--- src/components/Modal/useModal.js
+++ src/components/Modal/useModal.js
@@ -7,7 +7,12 @@ function useModal (modal) {
     return setIsVisible(!isVisible)
   }
 
-  return {isVisible, toggleModal}
+  // any other funtion that needs to be used with any modal can be added here
+  function sendData (data) {
+    // axios call to update the user info from the modal
+  }
+
+  return {isVisible, toggleModal, sendData}
 }
 
 export default useModal
