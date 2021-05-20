6c0ccc6ed801dd9d1223dad17c27ad6709eb3595
Germaine P <me@germainep.com>
2021-05-19 21:16:05 -0400

    feature: create view for editing user data

--- src/components/search-bar/StudentInfo.js
+++ src/components/search-bar/StudentInfo.js
@@ -1,32 +1,8 @@
+import { Button } from 'antd'
 import React from "react";
 
-const StudentInfo = () => {
-    const [student, setStudent] = React.useState({
-            "count": 0,
-            "next": "string",
-            "previous": "string",
-            "results": [
-              {
-                "id": "string",
-                "user": {
-                  "first_name": "string",
-                  "last_name": "string",
-                  "date_of_birth": "string",
-                  "time_zone": "string",
-                  "initials": "string",
-                  "personal_email": "user@example.com",
-                  "phone": "string",
-                  "city": "string",
-                  "notes": "string"
-                },
-                "created_date": "string",
-                "modified_date": "string",
-                "external_id": "string",
-                "school_level": 0,
-                "group_name": "string"
-              }
-            ]
-    })
+const StudentInfo = (props) => {
+    const [student, setStudent] = React.useState( props.user)
 
     const changeHandler = (e) => {
       e.persist();
@@ -36,132 +12,47 @@ const StudentInfo = () => {
       });
     };
 
+    const onSubmit = () => {
+      // this need to be connected to the backend to update the data on the database
+
+      props.closeEditing()
+    }
 return (
     <form>
-        {/* <label>
-            Count
-            <br></br>
-            <input 
-            placeholder={student.count}
-            name="count"
-            onChange={changeHandler}
-            value={student.count}
-            >
-            </input>
-        </label>
-        <br></br>
-        <label>
-            Next
-            <br></br>
-            <input 
-            placeholder={student.next}
-            name="next"
-            onChange={changeHandler}
-            value={student.next}
-            >
-            </input>
-        </label>
-        <label>
-           Previous
-           <br></br>
-            <input placeholder={student.previous}>
-            </input>
-        </label>
-        <br></br> */}
         <label>
             ID
             <br></br>
-            <input placeholder={student.results[0].id}>
-            </input>
+            <input  name='id' onChange={changeHandler} value={student.id}/>
+
         </label>
         <label>
             First Name
             <br></br>
-            <input placeholder={student.results[0].user.first_name}>
-            </input>
+            <input name='first_name' type="text" onChange={changeHandler} value={student.first_name}/>
         </label>
         <label>
             Last Name
             <br></br>
-            <input placeholder={student.results[0].user.last_name}>
-            </input>
+            <input name='last_name' onChange={changeHandler} value={student.last_name}/>
         </label>
         <br></br>
         <label>
             Date of Birth
             <br></br>
-            <input placeholder={student.results[0].user.date_of_birth}>
-            </input>
+            <input name='date_of_birth' onChange={changeHandler} value={student.date_of_birth}/>
         </label>
-        <label>
-            Time Zone
-            <br></br>
-            <input placeholder={student.results[0].user.time_zone}>
-            </input>
-        </label>
-        <label>
-            Initials
-            <br></br>
-            <input placeholder={student.results[0].user.initials}>
-            </input>
-        </label>
+      <label>
+        Personal Email
         <br></br>
+        <input name='personal_email' onChange={changeHandler} value={student.personal_email}/>
+      </label>
         <label>
-            Personal Email
+            Location
             <br></br>
-            <input placeholder={student.results[0].user.personal_email}>
-            </input>
+            <input name='city' onChange={changeHandler} value={student.city}/>
         </label>
-        <label>
-            Phone
-            <br></br>
-            <input placeholder={student.results[0].user.phone}>
-            </input>
-        </label>
-        <label>
-            City
-            <br></br>
-            <input placeholder={student.results[0].user.city}>
-            </input>
-        </label>
-        <br></br>
-        <label>
-            Notes
-            <br></br>
-            <input placeholder={student.results[0].user.notes}>
-            </input>
-        </label>
-        <label>
-            Created Date
-            <br></br>
-            <input placeholder={student.results[0].created_date}>
-            </input>
-        </label>
-        <label>
-            Motified Date
-            <br></br>
-            <input placeholder={student.results[0].modified_date}>
-            </input>
-        </label>
-        <br></br>
-        <label>
-            External ID
-            <br></br>
-            <input placeholder={student.results[0].external_id}>
-            </input>
-        </label>
-        <label>
-            School Level
-            <br></br>
-            <input placeholder={student.results[0].school_level}>
-            </input>
-        </label>
-        <label>
-            Group Name
-            <br></br>
-            <input placeholder={student.results[0].group_name}>
-            </input>
-        </label>
+
+      <Button onClick={onSubmit}>Save</Button>
     </form>
 )
 }
