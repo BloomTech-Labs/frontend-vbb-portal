e06fdf71e18c1ff61acad8fa71c8f75f40945b2f
Nadia Childs <nadiachilds@Nadias-MacBook-Air.local>
2021-05-26 21:29:57 -0400

    added mentee registration actions and starter reducer file

---
+++ src/redux/actions/MenteeRegistration.redux/MenteeRegistration.actions.js
@@ -0,0 +1,17 @@
+export const POST_FORM_DATA = 'POST_FORM_DATA';
+export const POST_FORM_DATA_SUCCESS = 'POST_FORM_DATA_SUCESS';
+export const POST_FORM_DATA_FAIL = 'POST_FORM_DATA_FAIL';
+
+export function postFormSuccess(successMessage) {
+  return {
+    type: POST_FORM_DATA_SUCCESS,
+    payload: successMessage,
+  };
+}
+
+export function postFormFail(errorMessage) {
+  return {
+    type: POST_FORM_DATA_FAIL,
+    payload: errorMessage,
+  };
+}
