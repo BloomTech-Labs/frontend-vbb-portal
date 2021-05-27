8c90ce54294b5357e28f11f9dd470b0e7fb07e77
Nadia Childs <nadiachilds@Nadias-MacBook-Air.local>
2021-05-26 22:00:34 -0400

    Made requested edits

--- src/redux/MenteeRegistration.redux/MenteeRegistration.actions.js
+++ src/redux/MenteeRegistration.redux/MenteeRegistration.actions.js
@@ -2,6 +2,12 @@ export const POST_FORM_DATA = 'POST_FORM_DATA';
 export const POST_FORM_DATA_SUCCESS = 'POST_FORM_DATA_SUCESS';
 export const POST_FORM_DATA_FAIL = 'POST_FORM_DATA_FAIL';
 
+export function postFormData(formData) {
+  return {
+    type: POST_FORM_DATA,
+    payload: formData,
+  };
+}
 export function postFormSuccess(successMessage) {
   return {
     type: POST_FORM_DATA_SUCCESS,
