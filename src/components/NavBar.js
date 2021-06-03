52cbf8b7342da2b90934ece8166716e05a3be82c
Daniel Terry <daniel.terry2121@gmail.com>
2021-06-02 21:12:03 -0400

    Basic MVP for search bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,41 +5,33 @@ import * as actions from '../redux/actions';
 import { Button, PageHeader } from 'antd';
 import { LoginOutlined, FormOutlined, LogoutOutlined } from '@ant-design/icons';
 import SearchBarAutoComplete from './search-bar/SearchBarAutoComplete';
-
+import SearchBar from './search/SearchBar'
 import fullLogo from '../images/vbb-full-logo.png';
+import useStyles from './styles';
 // import miniLogo from '../images/vbb-picture-logo.png';
 
-/**
- * NavBar
- * Connected functional component
- * Displays Navigation Header Bar
- *
- * @param { Type History } history from react router dom
- * @param {redux store} authToken authentication token from redux store
- * @param {redux action} logOut action from logout import as a connected component
- */
-const NavBar = ({ history, authToken, logOut }) => {
+const NavBar = ({ logout, authToken }) => {
+  const classes = useStyles();
   const signInSignOut = !authToken ? (
-    <Link to="/signin" key="link-signin">
-      <Button key="0" style={{ marginTop: '15px', color: '#549bea' }}>
+    <Link to="/signin" key="link-1">
+      <Button key="1" style={{ marginTop: '15px', color: '#549bea' }}>
         Sign In
         <LoginOutlined />
-      </Button>
+      </Button> 
     </Link>
   ) : (
-      <React.Fragment key="group">
-        <SearchBarAutoComplete key="searchBarAutoComplete"/>
-        <Link to="/" key="link-signout">
-          <Button
-            key="1"
-            style={{ marginTop: '15px', color: '#549bea' }}
-            onClick={logOut}
-          >
-            Sign Out
-            <LogoutOutlined />
-          </Button>
-        </Link>
-      </React.Fragment>
+      <>
+    <Link to="/" key="link-1">
+      <Button
+        key="1"
+        style={{ marginTop: '15px', color: '#549bea' }}
+        onClick={logout}
+      >
+        Sign Out
+        <LogoutOutlined />
+      </Button>
+    </Link>
+      </>
   );
   return (
     <PageHeader
@@ -47,10 +39,13 @@ const NavBar = ({ history, authToken, logOut }) => {
         position: 'fixed',
         zIndex: 1,
         width: '100%',
+        height: '200px',
         backgroundColor: '#ff914d',
       }}
       title={
-        <Link to="/" key="link-dashboard">
+        <Link to="/"
+          
+        >
           <img
             src={fullLogo}
             alt="Logo for Village Book Builders, small orange hut with Village Book Builders text"
@@ -60,14 +55,19 @@ const NavBar = ({ history, authToken, logOut }) => {
       }
       extra={[
         signInSignOut,
-        <Link to="/signup" key="link-signup">
+        <Link to="/signup" key="link-2">
           <Button type="primary" key="2">
             Register
             <FormOutlined style={{ color: 'white' }} />
           </Button>
         </Link>,
       ]}
-    ></PageHeader>
+    >
+
+      {authToken ? 
+         <SearchBar /> : null
+      }
+    </PageHeader>
   );
 };
 
@@ -77,3 +77,4 @@ const mapStateToProps = (state) => {
   };
 };
 export default withRouter(connect(mapStateToProps, actions)(NavBar));
+//austin.mehmet@villagebookbuilders.org
---
+++ src/components/search/SearchBar.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import SearchField from './SearchField'
+import { Input , Button, Modal } from 'antd';
+import { AudioOutlined } from '@ant-design/icons';
+import useModal from '../Modal/useModal';
+import SearchModalContent from '../Modal/SeachModalFragment';
+import useStyles from './styles';
+
+const SearchBar = () => {
+    const [value,setValue] = useState({});
+    const classes = useStyles();
+    const { Search } = Input;
+    const suffix = (
+        <AudioOutlined
+          style={{
+            fontSize: 16,
+            color: '#1890ff',
+          }}
+        />
+      );
+
+    return (
+        <>
+        <Search
+            type = 'text'
+            enterButton = "Search"
+            style = {{ width: "80%", margin: "0 100px" }}
+            onChange={(e) => setValue({ ...value, name: e.target.value })}
+        />
+        <SearchField  value = {value} />
+        </>
+    )
+}
+
+export default SearchBar
