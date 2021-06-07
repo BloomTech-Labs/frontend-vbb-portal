abaa3fc114cf760f93b598fc068cda2a01cafd98
Nadia Childs <nadiachilds@Nadias-Air.lan1>
2021-06-06 23:26:24 -0400

    made steps clickable for navigation

--- src/components/registration/MasterForm.js
+++ src/components/registration/MasterForm.js
@@ -4,7 +4,6 @@ import * as actions from '../../redux/actions';
 import { withRouter } from 'react-router';
 import { Button } from 'antd';
 import { RightOutlined, LeftOutlined, CheckOutlined } from '@ant-design/icons';
-
 import Step1 from './Step1';
 import Step2 from './Step2';
 import Step3 from './Step3';
@@ -17,6 +16,12 @@ const MasterForm = ({
   history,
 }) => {
   let [currentStep, setCurrentStep] = useState(1);
+
+  // allows users to navigate form by selectiving individual steps
+  const onChange = (currentStep) => {
+    console.log('onChange:', currentStep);
+    setCurrentStep(currentStep + 1);
+  };
 
   const next = () => {
     window.scrollTo(0, 0);
@@ -93,7 +98,7 @@ const MasterForm = ({
     <div>
       {currentStep < 4 ? (
         <div style={{ margin: '0 0 25px 0' }}>
-          <ProgressBar currentStep={currentStep} />
+          <ProgressBar currentStep={currentStep} onChange={onChange} />
         </div>
       ) : (
         <div></div>
