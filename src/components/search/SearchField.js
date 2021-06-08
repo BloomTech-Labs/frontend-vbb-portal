675d76644050d3d02454ad7d23511019282e0d98
Tim Dill <brodt7258@gmail.com>
2021-06-08 02:04:15 -0400

    refactor toggle and filter logic

--- src/components/search/SearchField.js
+++ src/components/search/SearchField.js
@@ -1,104 +1,134 @@
-import React,  { useState, useEffect } from 'react'
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Modal, Card } from 'antd';
+
 import useModal from '../Modal/useModal';
 import SearchModalContent from '../Modal/SeachModalFragment';
-import  data from '../search-bar/MOCK_DATA.json';
-import classes from  '../search-bar/Modal.css';
-import register from '../../registerServiceWorker';
+import data from '../search-bar/MOCK_DATA.json';
 
 const SearchField = ({ value }) => {
-    //state variables
-    const [ toggle, setToggle ] = useState(false);
-    const [ options, setOptions ] = useState();
-    const [ list, setList ] = useState([]);
-    //custom hooks
-    const {isVisible, selectedUser, toggleModal } = useModal(SearchModalContent)
-    
-   //custom made filter funtion without any hard specificity currently, I'd like to make this better
-    const filterData = (value,options) => {
-        setList([{}])
-        const newList =  options.filter((val) => {
-            const fullName = `${val.first_name.toLowerCase()} ${val.last_name.toLowerCase()}`;
-            if(val.first_name[0].toLowerCase().includes(value.name[0]?.toLowerCase()) || val.last_name[0].toLowerCase().includes(value.name[0]?.toLowerCase()))
-                if(fullName.includes(value.name.toLowerCase())) {
-            return val
+  //state variables
+  const [toggle, setToggle] = useState(false);
+  const [options, setOptions] = useState([]);
+  const [list, setList] = useState([]);
+  //custom hooks
+  const { isVisible, selectedUser, toggleModal } = useModal(Modal);
+
+  const filterData = (value, options) => {
+    if (!value?.name?.length) {
+      return [];
+    }
+    const searchTerm = value.name.toLowerCase();
+    return options
+      .filter((e) => {
+        if (
+          searchTerm.length <= e.first_name.length &&
+          searchTerm === e.first_name.slice(0, searchTerm.length).toLowerCase()
+        ) {
+          return true;
         }
-        }).filter((val, idx) => idx < 10).map((val) => val) 
-                return(
-                    newList
-                )
-
-      }
-
-    //wrote out the api call originally going to local host and everything seemed to work , reverted to local mockdata file for development
-
-      useEffect(() => {
-          setOptions(data);
-      },[]);
-      useEffect(() => {
-        if(toggle) {
-            if( value?.name?.length < 1 ) {
-                setToggle(!toggle)
-                setList(filterData(value,options))
-            }
-            else{
-             setList(filterData(value,options))
-            }
+        if (
+          searchTerm.length <= e.last_name.length &&
+          searchTerm === e.last_name.slice(0, searchTerm.length).toLowerCase()
+        ) {
+          return true;
         }
-        else{
-            if(value?.name?.length >= 1 ){
-            setToggle(!toggle)
-            setList(filterData(value,options))
-            }
-            else{
-                return null;
-            }
-        }
-       }, [value])
+        return false;
+      })
+      .slice(0, 10);
+  };
 
-       //setting up perma features first 
-       const features = [
-        {name: "Calendar", url: "/calendar/"},
-        {name: "Donate", url: "/donate/"},
-        {name: "Sign up", url: "/signup/"},
-        {name: "Sign in", url: "/signin/"},
-        {name: "Booking", url: "/booking/"},
-        {name: "Dashboard", url: "/"},
-        {name: "Register", url: "/register/"},
-        {name: "Create Mentor", url: ""}
-      ]
-      
-    return (
+  //wrote out the api call originally going to local host and everything seemed to work , reverted to local mockdata file for development
+  useEffect(() => {
+    setOptions(data);
+  }, []);
+
+  useEffect(() => {
+    if (value?.name?.length >= 1) {
+      setToggle(true);
+    } else {
+      setToggle(false);
+    }
+    setList(filterData(value, options));
+  }, [value, options]);
+
+  //setting up perma features first
+  const features = [
+    { name: 'Calendar', url: '/calendar/' },
+    { name: 'Donate', url: '/donate/' },
+    { name: 'Sign up', url: '/signup/' },
+    { name: 'Sign in', url: '/signin/' },
+    { name: 'Booking', url: '/booking/' },
+    { name: 'Dashboard', url: '/' },
+    { name: 'Register', url: '/register/' },
+    { name: 'Create Mentor', url: '' },
+  ];
+
+  return (
+    <>
+      {toggle && (
         <>
-        { toggle ?
-            <>
-            <Card 
-                style= {{backgroundColor: 'rgba(255,255,255,2.5)', width:"80%" , margin: "0 100px", overflow: "hidden", overflowY: "scroll", height: "20vh" }}
-            >
-                {list.map((e) => <li
-                                    className = 'searchBar'
-                                    onClick = {() => toggleModal(SearchModalContent, e)}
-                > {`${e.first_name} ${e.last_name}`} </li> )}
-                { value && list.length === 0 ?
-                        <p>Need to register a new mentee? click here <Link to = {register.url}> register </Link></p>
-                        : null
-                            }
-              
-                {features.map((feature) => <Link style = {{margin:"5px"}} to = {`${feature.url}`} onClick = {() => setToggle(false)}> {`${feature.name}`} </Link>) }
-              <Button type= "primary" danger onClick = {() => setToggle(false)} style = {{position:"relative", float:"right"}}> close </Button>
-            </Card>
-             <Modal visible={isVisible} onOk={toggleModal} onCancel={toggleModal} destroyOnClose={true} >
-             <SearchModalContent user={selectedUser}/>
-             <Button>Edit</Button>
-           </Modal>
-           </>
-             :null
-        }
-             
-        </>
-    )
-}
+          <Card
+            style={{
+              backgroundColor: 'rgba(255,255,255,2.5)',
+              width: '80%',
+              margin: '0 100px',
+              overflow: 'hidden',
+              overflowY: 'scroll',
+              height: '20vh',
+            }}
+          >
+            {list.map((e) => (
+              <li
+                key={e.id}
+                className="searchBar"
+                onClick={() => toggleModal(Modal, e)}
+              >
+                {' '}
+                {`${e.first_name} ${e.last_name}`}{' '}
+              </li>
+            ))}
+            {list.length === 0 && (
+              <p>
+                Need to register a new mentee? click here{' '}
+                <Link to={'/register/'}> register </Link>
+              </p>
+            )}
 
-export default SearchField
+            {features.map((feature) => (
+              <Link
+                key={feature.name}
+                style={{ margin: '5px' }}
+                to={`${feature.url}`}
+                onClick={() => setToggle(false)}
+              >
+                {' '}
+                {`${feature.name}`}{' '}
+              </Link>
+            ))}
+            <Button
+              type="primary"
+              danger
+              onClick={() => setToggle(false)}
+              style={{ position: 'relative', float: 'right' }}
+            >
+              {' '}
+              close{' '}
+            </Button>
+          </Card>
+          <Modal
+            visible={isVisible}
+            onOk={toggleModal}
+            onCancel={toggleModal}
+            destroyOnClose={true}
+          >
+            <SearchModalContent user={selectedUser} />
+            <Button>Edit</Button>
+          </Modal>
+        </>
+      )}
+    </>
+  );
+};
+
+export default SearchField;
