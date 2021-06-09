e26e91daa7dbf7e1434a985a3aebd66815c55bae
Tim Dill <brodt7258@gmail.com>
2021-06-09 00:44:38 -0400

    lifted toggle and listening logic out to SearchBar component

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useRef, useEffect } from 'react';
 import SearchField from './SearchField';
 import { Input } from 'antd';
 
@@ -6,16 +6,53 @@ const { Search } = Input;
 
 const SearchBar = () => {
   const [value, setValue] = useState({ name: '' });
+  const [toggle, setToggle] = useState(false);
+
+  const barRef = useRef();
+  const fieldRef = useRef();
+
+  const handleClickSearchBar = () => {
+    console.log('click');
+    setToggle(true);
+  };
+
+  const handleClickOutside = useCallback(
+    (event) => {
+      if (
+        !barRef.current?.contains(event.target) &&
+        !fieldRef.current?.contains(event.target)
+      ) {
+        setToggle(false);
+      }
+    },
+    [setToggle]
+  );
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, false);
+    return () => {
+      document.removeEventListener('click', handleClickOutside, false);
+    };
+  }, [handleClickOutside]);
 
   return (
     <>
-      <Search
-        type="text"
-        enterButton="Search"
-        style={{ width: '80%', margin: '0 100px' }}
-        onChange={(e) => setValue({ ...value, name: e.target.value })}
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div ref={barRef} style={{ width: '80%', margin: '0 100px' }}>
+          <Search
+            type="text"
+            enterButton="Search"
+            onChange={(e) => setValue({ ...value, name: e.target.value })}
+            onClick={handleClickSearchBar}
+          />
+        </div>
+      </div>
+      <SearchField
+        value={value}
+        toggle={toggle}
+        setToggle={setToggle}
+        fieldRef={fieldRef}
       />
-      <SearchField value={value} />
     </>
   );
 };
