7a5c11904326840bd3f77a5c4e4ae976295f7c0f
Michael Sanchez <michaelsanchez42@live.com>
2021-06-08 21:54:50 -0600

    updated with prettier document format

--- src/redux/StudentEdit.redux/StudentEdit.reducer.js
+++ src/redux/StudentEdit.redux/StudentEdit.reducer.js
@@ -1,33 +1,35 @@
-import { EDIT_USER_START, EDIT_USER_SUCCESS, EDIT_USER_FAILURE} from './StudentEdit.action'
-
+import {
+  EDIT_USER_START,
+  EDIT_USER_SUCCESS,
+  EDIT_USER_FAILURE,
+} from './StudentEdit.action';
 
 const userEditInitialState = {
-    editingUser: [],
-    editing: false,
-    error: ""
-}
+  editingUser: [],
+  editing: false,
+  error: '',
+};
 
 export const editUser = (state = userEditInitialState, action) => {
-    switch (action.type) {
-        case EDIT_USER_START:
-            return {
-                ...state,
-                editing: true
-            }
-            case EDIT_USER_SUCCESS:
-            return {
-                ...state,
-                editing: false,
-                editingUser: action.payload
-
-            }
-            case EDIT_USER_FAILURE:
-            return {
-                ...state,
-                editing: false,
-                error: action.payload
-            }
-            default:
-                return state;
-    }
-}
+  switch (action.type) {
+    case EDIT_USER_START:
+      return {
+        ...state,
+        editing: true,
+      };
+    case EDIT_USER_SUCCESS:
+      return {
+        ...state,
+        editing: false,
+        editingUser: action.payload,
+      };
+    case EDIT_USER_FAILURE:
+      return {
+        ...state,
+        editing: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
