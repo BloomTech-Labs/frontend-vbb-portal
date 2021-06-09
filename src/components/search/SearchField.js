12402f268856a65b22c8be0f1bfe97694ffba3ca
Tim Dill <brodt7258@gmail.com>
2021-06-09 00:44:38 -0400

    lifted toggle and listening logic out to SearchBar component

--- src/components/search/SearchField.js
+++ src/components/search/SearchField.js
@@ -1,27 +1,20 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Modal, Card } from 'antd';
 
-import  '../search-bar/Modal.css';
+import '../search-bar/Modal.css';
 import useModal from '../Modal/useModal';
 import SearchModalContent from '../Modal/SeachModalFragment';
 import data from '../search-bar/MOCK_DATA.json';
 
-const SearchField = ({ value }) => {
+const SearchField = ({ value, toggle, setToggle, fieldRef }) => {
   //state variables
-  const [toggle, setToggle] = useState(false);
   const [options, setOptions] = useState([]);
   const [list, setList] = useState([]);
   //custom hooks
   const { isVisible, selectedUser, toggleModal } = useModal(Modal);
-  const wrapperRef = useRef(null);
 
   //click outside function
-  const handleClickOutside = event => {
-    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
-      setToggle(false);
-    }
-  };
 
   //filter function for search bar
   const filterData = (value, options) => {
@@ -54,22 +47,10 @@ const SearchField = ({ value }) => {
   }, []);
 
   useEffect(() => {
-    
-    if (value?.name?.length >= 1) {
-      setToggle(true);
-    } else {
-      setToggle(false);
-    }
     setList(filterData(value, options));
-  }, [value, options]);
+  }, [value, options, setToggle]);
 
   //use Effect for click listener
-  useEffect(() => {
-    document.addEventListener("click", handleClickOutside, false);
-    return () => {
-      document.removeEventListener("click", handleClickOutside, false);
-    };
-  }, [toggle]);
 
   //setting up perma features first
   const features = [
@@ -83,58 +64,62 @@ const SearchField = ({ value }) => {
     { name: 'Create Mentor', url: '' },
   ];
 
+  console.log('toggle', toggle);
   return (
     <>
       {toggle && (
-        <><div ref = {wrapperRef}>
-          <Card
-            style={{
-              backgroundColor: 'rgba(255,255,255,2.5)',
-              width: '80%',
-              margin: '0 100px',
-              overflow: 'hidden',
-              overflowY: 'scroll',
-              height: '20vh',
-            }}
-          >
-            {list.map((e) => (
-              <li
-                key={e.id}
-                className="searchBar"
-                onClick={() => toggleModal(Modal, e)}
-              >
-                {' '}
-                {`${e.first_name} ${e.last_name}`}{' '}
-              </li>
-            ))}
-            {list.length === 0 && (
-              <p>
-                Need to register a new mentee? click here{' '}
-                <Link to={'/register/'}> register </Link>
-              </p>
-            )}
-
-            {features.map((feature) => (
-              <Link
-                key={feature.name}
-                style={{ margin: '5px' }}
-                to={`${feature.url}`}
-                onClick={() => setToggle(false)}
-              >
-                {' '}
-                {`${feature.name}`}{' '}
-              </Link>
-            ))}
-            <Button
-              type="primary"
-              danger
-              onClick={() => setToggle(false)}
-              style={{ position: 'relative', float: 'right' }}
+        <div
+          style={{ width: '100%', display: 'flex', justifyContent: 'center' }}
+        >
+          <div ref={fieldRef} style={{ width: '80%' }}>
+            <Card
+              style={{
+                backgroundColor: 'rgba(255,255,255,2.5)',
+                width: '100%',
+                margin: '0',
+                overflow: 'hidden',
+                overflowY: 'scroll',
+                height: '20vh',
+              }}
             >
-              {' '}
-              close{' '}
-            </Button>
-          </Card>
+              {list.map((e) => (
+                <li
+                  key={e.id}
+                  className="searchBar"
+                  onClick={() => toggleModal(Modal, e)}
+                >
+                  {' '}
+                  {`${e.first_name} ${e.last_name}`}{' '}
+                </li>
+              ))}
+              {list.length === 0 && (
+                <p>
+                  Need to register a new mentee? click here{' '}
+                  <Link to={'/register/'}> register </Link>
+                </p>
+              )}
+
+              {features.map((feature) => (
+                <Link
+                  key={feature.name}
+                  style={{ margin: '5px' }}
+                  to={`${feature.url}`}
+                  onClick={() => setToggle(false)}
+                >
+                  {' '}
+                  {`${feature.name}`}{' '}
+                </Link>
+              ))}
+              <Button
+                type="primary"
+                danger
+                onClick={() => setToggle(false)}
+                style={{ position: 'relative', float: 'right' }}
+              >
+                {' '}
+                close{' '}
+              </Button>
+            </Card>
           </div>
           <Modal
             visible={isVisible}
@@ -145,7 +130,7 @@ const SearchField = ({ value }) => {
             <SearchModalContent user={selectedUser} />
             <Button>Edit</Button>
           </Modal>
-        </>
+        </div>
       )}
     </>
   );
