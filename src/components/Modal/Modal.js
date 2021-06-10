ff3fe241432da0cdf124bdd625ca965697531d53
Tim Dill <brodt7258@gmail.com>
2021-06-09 23:43:37 -0400

    cleanup imports and logging statements

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,10 +1,9 @@
+import React from 'react';
 import { Modal as AntdModal } from 'antd';
 import { connect } from 'react-redux';
 import { closeModal } from '../../redux/actions';
 
 const Modal = ({ content, isOpen, closeModal, config }) => {
-  console.log('modal info', { content, isOpen, closeModal });
-  console.log(config);
   return (
     <AntdModal
       visible={isOpen}
