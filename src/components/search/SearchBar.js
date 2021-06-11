80f4742141b35e1b598de8b10acd7c8f5e15a4d8
Tim Dill <brodt7258@gmail.com>
2021-06-11 09:07:13 -0400

    refactor SearchBar, use new api, break out child components

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -1,31 +1,31 @@
-import React, { useState, useCallback, useRef, useEffect } from 'react';
-import SearchField from './SearchField';
+import { useState, useCallback, useRef, useEffect } from 'react';
 import { Input } from 'antd';
+
+import SearchField from './SearchField';
+import { searchUsers } from '../../mock-data/mockApi';
 
 const { Search } = Input;
 
 const SearchBar = () => {
   const [value, setValue] = useState({ name: '' });
   const [toggle, setToggle] = useState(false);
+  const [results, setResults] = useState({ student: [], mentor: [] });
 
   const barRef = useRef();
   const fieldRef = useRef();
 
-  const handleClickSearchBar = () => {
-    setToggle(true);
-  };
+  useEffect(() => {
+    searchUsers(value.name).then((data) => setResults(data));
+  }, [value]);
 
-  const handleClickOutside = useCallback(
-    (event) => {
-      if (
-        !barRef.current?.contains(event.target) &&
-        !fieldRef.current?.contains(event.target)
-      ) {
-        setToggle(false);
-      }
-    },
-    [setToggle]
-  );
+  const handleClickOutside = useCallback((event) => {
+    if (
+      !barRef.current?.contains(event.target) &&
+      !fieldRef.current?.contains(event.target)
+    ) {
+      setToggle(false);
+    }
+  }, []);
 
   useEffect(() => {
     document.addEventListener('click', handleClickOutside, false);
@@ -35,31 +35,28 @@ const SearchBar = () => {
   }, [handleClickOutside]);
 
   return (
-    <>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <div ref={barRef} style={{ width: '80%', margin: '0 100px' }}>
-          <Search
-            type="text"
-            enterButton="Search"
-            onChange={(e) => setValue({ ...value, name: e.target.value })}
-            onClick={handleClickSearchBar}
-          />
-        </div>
-        <SearchField
-          value={value}
-          toggle={toggle}
-          setToggle={setToggle}
-          fieldRef={fieldRef}
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div ref={barRef} style={{ width: '80%' }}>
+        <Search
+          type="text"
+          enterButton="Search"
+          onChange={(e) => setValue({ ...value, name: e.target.value })}
+          onClick={() => setToggle(true)}
         />
       </div>
-    </>
+      {toggle && (
+        <div ref={fieldRef} style={{ width: '80%' }}>
+          <SearchField results={results} setToggle={setToggle} />
+        </div>
+      )}
+    </div>
   );
 };
 
--- src/components/search/SearchField.js
+++ src/components/search/SearchField.js
@@ -1,53 +1,11 @@
-import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Card } from 'antd';
-import { connect } from 'react-redux';
 
 import '../../less/Modal.less';
-import StudentInfoModal from '../StudentInfo/StudentInfoModal';
-import data from '../../mock-data/MOCK_DATA.json';
-import { createModal } from '../../redux/actions';
+import SearchResultsList from './SearchResultsList';
 
-const SearchField = ({ value, toggle, setToggle, fieldRef, createModal }) => {
-  //state variables
-  const [options, setOptions] = useState([]);
-  const [list, setList] = useState([]);
-
-  //filter function for search bar
-  const filterData = (value, options) => {
-    if (!value?.name?.length) {
-      return [];
-    }
-    const searchTerm = value.name.toLowerCase();
-    return options
-      .filter((e) => {
-        if (
-          searchTerm.length <= e.first_name.length &&
-          searchTerm === e.first_name.slice(0, searchTerm.length).toLowerCase()
-        ) {
-          return true;
-        }
-        if (
-          searchTerm.length <= e.last_name.length &&
-          searchTerm === e.last_name.slice(0, searchTerm.length).toLowerCase()
-        ) {
-          return true;
-        }
-        return false;
-      })
-      .slice(0, 10);
-  };
-
-  //wrote out the api call originally going to local host and everything seemed to work , reverted to local mockdata file for development
-  useEffect(() => {
-    setOptions(data);
-  }, []);
-
-  useEffect(() => {
-    setList(filterData(value, options));
-  }, [value, options, setToggle]);
-
-  //use Effect for click listener
+const SearchField = ({ results, setToggle }) => {
+  console.log('SearchField', results);
 
   //setting up perma features first
   const features = [
@@ -62,51 +20,41 @@ const SearchField = ({ value, toggle, setToggle, fieldRef, createModal }) => {
   ];
 
   return (
-    <>
-      {toggle && (
-        <div ref={fieldRef} style={{ width: '80%' }}>
-          <Card
-            style={{
-              backgroundColor: 'rgba(255,255,255,2.5)',
-              width: '100%',
-              margin: '0',
-              overflow: 'hidden',
-              overflowY: 'scroll',
-              height: '20vh',
-            }}
-          >
-            {list.map((e) => (
-              <li
-                key={e.id}
-                className="searchBar"
-                onClick={() => createModal(<StudentInfoModal user={e} />)}
-              >
-                {' '}
-                {`${e.first_name} ${e.last_name}`}{' '}
-              </li>
-            ))}
-            {list.length === 0 && (
-              <p>
-                Need to register a new mentee? click here{' '}
-                <Link to={'/register/'}> register </Link>
-              </p>
-            )}
-            {features.map((feature) => (
-              <Link
-                key={feature.name}
-                style={{ margin: '5px' }}
-                to={`${feature.url}`}
-                onClick={() => setToggle(false)}
-              >
-                {' '}
-                {`${feature.name}`}{' '}
-              </Link>
-            ))}
-          </Card>
-        </div>
+    <Card
+      style={{
+        backgroundColor: 'rgba(255,255,255,2.5)',
+        width: '100%',
+        margin: '0',
+        overflow: 'hidden',
+        overflowY: 'scroll',
+        maxHeight: '40vh',
+      }}
+    >
+      {!!results.student.length && (
+        <SearchResultsList title="Students" results={results.student} />
       )}
-    </>
+      {!!results.mentor.length && (
+        <SearchResultsList title="Mentors" results={results.mentor} />
+      )}
+      {Object.values(results).every((e) => !e.length) && (
+        <p>
+          Need to register a new mentee? click here{' '}
+          <Link to={'/register/'}> register </Link>
+        </p>
+      )}
+      {features.map((feature) => (
+        <Link
+          key={feature.name}
+          style={{ margin: '5px' }}
+          to={`${feature.url}`}
+          onClick={() => setToggle(false)}
+        >
+          {' '}
+          {`${feature.name}`}{' '}
+        </Link>
+      ))}
+    </Card>
   );
 };
 
-export default connect(null, { createModal })(SearchField);
+export default SearchField;
