a52f6129e1ca7ea45bc01a1f728bbb7e8dd66ba2
Tim Dill <brodt7258@gmail.com>
2021-06-16 06:18:25 -0400

    create useDebounce hook

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,32 @@
+import { useState, useEffect, useRef } from 'react';
+
+export const useDebounce = (
+  fn,
+  initVal = null,
+  initRes = null,
+  delay = 400
+) => {
+  const [value, setValue] = useState(initVal);
+  const [output, setOutput] = useState({ result: initRes, error: null });
+
+  const fnRef = useRef();
+
+  useEffect(() => {
+    fnRef.current = fn;
+  }, [fn]);
+
+  useEffect(() => {
+    async function cb() {
+      try {
+        const result = await fnRef.current(value);
+        setOutput({ result, error: null });
+      } catch (error) {
+        setOutput({ result: initRes, error });
+      }
+    }
+    const id = setTimeout(cb, delay);
+    return () => clearTimeout(id);
+  }, [delay, value]);
+
+  return [output, setValue];
+};
