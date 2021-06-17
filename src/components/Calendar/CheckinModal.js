f2838db6b1d156b453f63c6b6202e6ccd1ad30fe
patrickb42 <patrickbaldwin@gmail.com>
2021-06-17 19:54:18 -0400

    added logic to check in a student if it's at least 30 minutes before the start time

--- src/components/Calendar/CheckinModal.js
+++ src/components/Calendar/CheckinModal.js
@@ -1,86 +1,109 @@
 import React from 'react'
 import { Modal, Button, Switch } from 'antd'
 import { CheckCircleOutlined, CheckCircleTwoTone } from '@ant-design/icons';
+
 import '../../less/calendar.less'
 
-const CheckinModal = (props) => {
+const CheckinModal = ({
+  details,
+  isModalVisiable,
+  setClickSelected,
+  setIsModalVisible,
+}) => {
+  const {
+    checkedIn,
+    end,
+    mentor,
+    resourceId,
+    start,
+    student,
+    title,
+  } = details;
 
-    if(!props.details.start) {
-        return(
-            <>
-                loading
-            </>
-        )
-    }
+  if(!start) {
+      return(
+          <>
+              loading
+          </>
+      )
+  }
 
-    console.log(props.details)
+  console.log(details)
 
-    const handleOk = () => {
-        props.setIsModalVisible(false);
-      };
-    
-      const handleCancel = () => {
-        props.setIsModalVisible(false);
-      };
-    
-    const day = props.details.start.toLocaleDateString()
-    const start = props.details.start.toLocaleTimeString()
-    const end = props.details.end.toLocaleTimeString()
-
-
-    const checkedIn = props.details.checkedIn
-
-    const handleCheckIn = () => {
-        props.setClickSelected({
-            ...props.details,
-            checkedIn: !props.details.checkedIn
-        })
-        console.log(props.details)
-    }
+  const handleOk = () => {
+      setIsModalVisible(false);
+    };
+  
+    const handleCancel = () => {
+      setIsModalVisible(false);
+    };
+  
+  const dayString = start.toLocaleDateString()
+  const startString = start.toLocaleTimeString()
+  const endString = end.toLocaleTimeString()
 
 
-    return (
-        <Modal 
-            visible={props.isModalVisiable}
-            onOk={handleOk}
-            onCancel={handleCancel}
-            style={{ textAlign: "center"}}
-        >
-            <h6 style={{
-                textTransform: 'uppercase', 
-                fontSize: '.75rem', 
-                paddingBottom: '1rem', 
-                color: props.details.checkedIn ? '#52c41a' : 'red'
-                }}>
-                    Status: {checkedIn ? 'checked-in' : 'not checked-in'}</h6>
-          <h2>{props.details.title}</h2>
-            {
-          //If the details haven't loaded yet do not display, to handle toLocalDateString() error
-            day &&
-            <p>
-                <strong>When: </strong> 
-                {day} {start} - {end}
-            </p>
-            }
-            <p>
-                <strong>Mentor: </strong> 
-                {props.details.mentor} 
-            </p>
-            <p> 
-                <strong>Student: </strong> 
-                {props.details.student}
-            </p>
-            <p>
-              <strong>Computer: </strong> 
-              {props.details.resourceId}
-            </p>
-          <Switch
-            checked={checkedIn} 
-            checkedChildren="Student Checked In" unCheckedChildren="Check-in Student"
-            onChange={handleCheckIn}
-        />
-      </Modal>
-    )
+  const isCheckedIn = checkedIn
+
+  const handleCheckIn = () => {
+      setClickSelected({
+          ...details,
+          checkedIn: !checkedIn
+      })
+      console.log(details)
+  }
+
+  const isAtLeastXMinutesToCurTime = ({ date, x }) => {
+    const dateInMS = date.getTime();
+    const MILLISECONDS_IN_A_MINUTE = 60_000;
+
+    return dateInMS + (x * MILLISECONDS_IN_A_MINUTE) >= Date.now();
+  };
+
+
+  return (
+      <Modal 
+          visible={isModalVisiable}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          style={{ textAlign: "center"}}
+      >
+          <h6 style={{
+              textTransform: 'uppercase', 
+              fontSize: '.75rem', 
+              paddingBottom: '1rem', 
+              color: checkedIn ? '#52c41a' : 'red'
+              }}>
+                  Status: {isCheckedIn ? 'checked-in' : 'not checked-in'}</h6>
+        <h2>{title}</h2>
+          {
+        //If the details haven't loaded yet do not display, to handle toLocalDateString() error
+          dayString &&
+          <p>
+              <strong>When: </strong> 
+              {dayString} {startString} - {endString}
+          </p>
+          }
+          <p>
+              <strong>Mentor: </strong> 
+              {mentor} 
+          </p>
+          <p> 
+              <strong>Student: </strong> 
+              {student}
+          </p>
+          <p>
+            <strong>Computer: </strong> 
+            {resourceId}
+          </p>
+        <Switch
+          checked={isCheckedIn} 
+          checkedChildren="Student Checked In" unCheckedChildren="Check-in Student"
+          disabled={isAtLeastXMinutesToCurTime({ date: start, x: 30 })}
+          onChange={handleCheckIn}
+      />
+    </Modal>
+  )
 }
 
 export default CheckinModal
