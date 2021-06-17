b6815efdaab9cc0fc4650938371a3c5f248e8cd3
Sean Kramer <seandominickramer@gmail.com>
2021-06-16 21:51:10 -0400

    Add actions to add a user

---
+++ src/redux/admin/AddUser/AddUser.actions.js
@@ -0,0 +1,37 @@
+export const ADD_USER_REQUEST = "ADD_USER_REQUEST"
+export const ADD_USER_SUCCESS = "ADD_USER_SUCCESS"
+export const ADD_USER_FAILURE = "ADD_USER_FAILURE"
+
+export const AddUserRequest = () => {
+  return {
+    type: ADD_USER_REQUEST
+  }
+}
+
+export const AddUserSuccess = (NewUser) => {
+  return {
+    type: ADD_USER_SUCCESS,
+    payload: NewUser
+   
+  }
+}
+
+export const AddUserFailure = (error) => {
+  return {
+    type: ADD_USER_FAILURE,
+    payload: error
+  }
+}
+
+export const AddUser = (id, NewUser) =>{
+return function (dispatch) {
+  dispatch(AddUserRequest())
+  axiosWithAuth()
+.post(`/api/`, NewUser)
+  .then((res) => {
+    dispatch(AddUserSuccess())
+  })
+  .catch(err => {
+    dispatch(AddUserFailure(err.message))
+  })
+}}
