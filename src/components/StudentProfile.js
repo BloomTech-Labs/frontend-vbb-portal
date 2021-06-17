5c7a9f5664b6fb5b7a248756706cf0216b36c8e6
spazdrum <spazdrum@gmail.com>
2021-06-16 23:39:14 -0400

    Replaced buttons with cards

--- src/components/StudentProfile.js
+++ src/components/StudentProfile.js
@@ -3,6 +3,14 @@ import axios from 'axios';
 import * as actions from '../redux/actions';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { Card } from 'react-bootstrap';
+
+import Whiteboard from '../images/Whiteboard.png';
+import Khan from '../images/Khan-Academy.png';
+import Classroom from '../images/google-classroom.jpg';
+import Facebook from '../images/VBB-FB.png';
+import Meet from '../images/google-meet.jpg';
+import Website from '../images/VBB-Website.png';
 
 class Student extends React.Component {
   state = {
@@ -66,81 +74,222 @@ class Student extends React.Component {
                   color: '#ff914d',
                 }}
               >
-                <b>Welcome Student, </b> please check out the available resources to help you along your journey
+                <b>Welcome Student, </b> please check out the available
+                resources to help you along your journey
               </h4>
             </>
           )}
         </div>
         <div className="column col-card" id="student-resources-box">
-          <div className="twocol-container" id="student-resources-fields">
-            <a 
-              className="btn btn-light dashboard-btn l6-btn"
-              href="https://idroo.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Whiteboard
-            </a>
-            <a style={{
-              marginLeft: '10px'
+          <div
+            className="twocol-container"
+            id="student-resources-fields"
+            style={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              justifyContent: 'center',
             }}
-              className="btn btn-light dashboard-btn l2-btn"
-              href="https://www.khanacademy.org/"
-              target="_blank"
-              rel="noopener noreferrer"
+          >
+            <Card
+              style={{
+                width: '18rem',
+                margin: '1rem',
+                boxShadow: '0 2px 7px 3px #949ca5',
+              }}
             >
-              Khan Academy
-            </a>
-            <a style={{
-              marginLeft: '10px'
-            }}
-              className="btn btn-light dashboard-btn l4-btn"
-              href="https://classroom.google.com/u/0/h"
-              target="_blank"
-              rel="noopener noreferrer"
+              <a
+                className="card-image-link"
+                href="https://idroo.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Card.Img
+                  variant="top"
+                  src={Whiteboard}
+                  alt="Whiteboard"
+                  style={{ maxWidth: 'auto', height: 'auto' }}
+                />
+              </a>
+              <Card.Body>
+                <Card.Title>Whiteboard</Card.Title>
+                <Card.Text>
+                  <small>
+                    IDroo does everything you would expect from an online
+                    whiteboard. Instant real time collaboration in your browser
+                    without the hassle.
+                  </small>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card
+              style={{
+                width: '18rem',
+                margin: '1rem',
+                boxShadow: '0 2px 7px 3px #949ca5',
+              }}
             >
-              Google Classroom
-            </a>
-            <a style={{
-              marginLeft: '10px'
-            }}
-              className="btn btn-light dashboard-btn l3-btn"
-              href="https://www.facebook.com/VillageBookBuilders/"
-              target="_blank"
-              rel="noopener noreferrer"
+              <a
+                className="card-image-link"
+                href="https://www.khanacademy.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Card.Img
+                  variant="top"
+                  src={Khan}
+                  alt="Khan Academy"
+                  // style={{ maxWidth: '250px', height: 'auto' }}
+                />
+              </a>
+              <Card.Body>
+                <Card.Title>Khan Academy</Card.Title>
+                <Card.Text>
+                  <small>
+                    Khan Academy offers practice exercises, instructional
+                    videos, and a personalized learning dashboard that empower
+                    learners to study at their own pace in and outside of the
+                    classroom.
+                  </small>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card
+              style={{
+                width: '18rem',
+                margin: '1rem',
+                boxShadow: '0 2px 7px 3px #949ca5',
+              }}
             >
-              Village Mentors Facebook Page
-            </a>
-            <a style={{
-              marginLeft: '10px'
-            }}
-              className="btn btn-light dashboard-btn l6-btn"
-              href="https://apps.google.com/meet/"
-              target="_blank"
-              rel="noopener noreferrer"
+              <a
+                className="card-image-link"
+                href="https://classroom.google.com/u/0/h"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Card.Img
+                  variant="top"
+                  src={Classroom}
+                  alt="Google Classroom"
+                  // style={{ maxWidth: '250px', height: 'auto' }}
+                />
+              </a>
+              <Card.Body>
+                <Card.Title>Google Classroom</Card.Title>
+                <Card.Text>
+                  <small>
+                    Google Classroom is a free web service developed by Google
+                    for schools that aims to simplify creating, distributing,
+                    and grading assignments. The primary purpose of Google
+                    Classroom is to streamline the process of sharing files
+                    between teachers and students.
+                  </small>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card
+              style={{
+                width: '18rem',
+                margin: '1rem',
+                boxShadow: '0 2px 7px 3px #949ca5',
+              }}
             >
-              Google Meets
-            </a>
-            <a style={{
-              marginLeft: '10px'
-            }}
-              className="btn btn-light dashboard-btn l8-btn"
-              href="https://www.villagebookbuilders.org/"
-              target="_blank"
-              rel="noopener noreferrer"
+              <a
+                className="card-image-link"
+                href="https://www.facebook.com/VillageBookBuilders/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Card.Img
+                  variant="top"
+                  src={Facebook}
+                  alt="Village Facebook"
+                  // style={{ maxWidth: '250px', height: 'auto' }}
+                />
+              </a>
+              <Card.Body>
+                <Card.Title>Village Facebook</Card.Title>
+                <Card.Text>
+                  <small></small>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card
+              style={{
+                width: '18rem',
+                margin: '1rem',
+                boxShadow: '0 2px 7px 3px #949ca5',
+              }}
             >
-              VBB Home Website
-            </a>
+              <a
+                className="card-image-link"
+                href="https://apps.google.com/meet/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Card.Img
+                  variant="top"
+                  src={Meet}
+                  alt="Google Meet"
+                  // style={{ maxWidth: '250px', height: 'auto' }}
+                />
+              </a>
+              <Card.Body>
+                <Card.Title>Google Meet</Card.Title>
+                <Card.Text>
+                  <small>
+                    Google Meet is a video-communication service developed by
+                    Google. It is one of two apps that constitute the
+                    replacement for Google Hangouts, the other being Google
+                    Chat.
+                  </small>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card
+              style={{
+                width: '18rem',
+                margin: '1rem',
+                boxShadow: '0 2px 7px 3px #949ca5',
+              }}
+            >
+              <a
+                className="card-image-link"
+                href="https://www.villagebookbuilders.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Card.Img
+                  variant="top"
+                  src={Website}
+                  alt="Home Website"
+                  // style={{ maxWidth: 'auto', height: 'auto' }}
+                />
+              </a>
+              <Card.Body>
+                <Card.Title>Village Home Website</Card.Title>
+                <Card.Text>
+                  <small>
+                    We are a non-profit organization EMPOWERING villages around
+                    the world to end the cycle of poverty through EDUCATION.
+                    Education plays a big role in the development of a community
+                    and BUILDING a library is just the first step towards
+                    creating a space dedicated to LEARNING
+                  </small>
+                </Card.Text>
+              </Card.Body>
+            </Card>
           </div>
           <p
             style={{ padding: '20px', paddingLeft: '40px', maxWidth: '900px' }}
           >
             <b>
-              If you would like to change a mentoring session, have questions about
-              mentoring, or ANY QUESTIONS, please contact your mentor advisor at{' '}
+              If you would like to change a mentoring session, have questions
+              about mentoring, or ANY QUESTIONS, please contact your mentor
+              advisor at{' '}
               <a href="mailto:mentor@villagebookbuilders.org">
                 mentor@villagebookbuilders.org
-              </a>!<br />
+              </a>
+              !<br />
             </b>
           </p>
         </div>
