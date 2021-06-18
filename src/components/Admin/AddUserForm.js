6892469713d2ebd268614a1b02dc91e72eaa6655
dtebo <darrentebo83@outlook.com>
2021-06-18 18:50:59 -0400

    added activate/deactivate switch to the edit user form

--- src/components/Admin/AddUserForm.js
+++ src/components/Admin/AddUserForm.js
@@ -3,6 +3,7 @@ import {
   Divider,
   Form,
   Input,
+  Switch,
   Button
 } from 'antd';
 
@@ -16,16 +17,34 @@ const buttonLayout = {
     wrapperCol: { offset: 12, span: 16 },
 };
 
+const switchLayout = {
+    wrapperCol: { offset: 20 },
+};
+
 
 /*End Form Styling*/
 const AddUserForm = props => {
     const { EditMode, Record } = props;
+
+    const onChange = checked => {
+        console.log(`switch to ${checked}`)
+    };
 
     return(
         <div>
             <Form
                 {...layout}
                 name= "basic">
+                {EditMode ? (<Form.Item {...switchLayout}>
+                                <Switch
+                                    defaultChecked
+                                    checkedChildren="Activated"
+                                    unCheckedChildren="Deactivated"
+                                    onChange={onChange}
+                                />
+                            </Form.Item>) : (
+                                <></>
+                            )}
                 <Form.Item
                     label="First Name"
                     name="fname"
