e8bdcb5e9b1e78405579fc84e35675bf6c15736e
Sean Kramer <seandominickramer@gmail.com>
2021-06-21 21:18:02 -0400

    Add reducer to redux folder for admin to add user

--- src/redux/Admin/AddUser.actions.js
+++ src/redux/Admin/AddUser.actions.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 export const ADD_USER_REQUEST = "ADD_USER_REQUEST"
 export const ADD_USER_SUCCESS = "ADD_USER_SUCCESS"
 export const ADD_USER_FAILURE = "ADD_USER_FAILURE"
