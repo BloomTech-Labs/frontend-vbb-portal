7e0fd457d48f7ae0d81083882181c2ef9f2289b1
dtebo <darrentebo83@outlook.com>
2021-06-23 10:41:20 -0400

    added and implemented useForm hook on the add/edit user form

---
+++ src/hooks/useForm.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+export default function useForm(initialValues){
+    const [values, setValues] = useState(initialValues);
+
+    const handleChanges = e => {
+        e.persist();
+
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        });
+
+        console.log(values);
+    };
+
+    const clearForm = () => {
+        setValues(initialValues);
+    };
+
+    return [values, handleChanges, clearForm];
+}
