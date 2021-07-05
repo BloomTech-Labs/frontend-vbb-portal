2a8b4cfd7a8de1800c460a9f142573291354d7fd
dtebo <darrentebo83@outlook.com>
2021-07-05 19:34:48 -0400

    added email field to the add/edit user form

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -4,11 +4,11 @@ export default function useForm(initialValues){
     const [values, setValues] = useState(initialValues);
 
     const handleChanges = e => {
-        e.persist();
-
+        if(e.target.type !== "button") e.persist();
+console.log(e);
         setValues({
             ...values,
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.name === "activate" ? [e.target.value] : e.target.value
         });
 
         console.log(values);
