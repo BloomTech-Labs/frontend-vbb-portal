80afc8d2f5456a1a7efc758ca60e07728845d329
Angelika Blackburn <esonyei@gmail.com>
2021-07-07 16:26:34 -0400

    Added search component to header and removed clickable link from breadcrumb, making search feature only access to admin dashboard

--- src/containers/MainLayout.js
+++ src/containers/MainLayout.js
@@ -5,6 +5,7 @@ import { Layout, Breadcrumb } from 'antd';
 import Routes from './Routes';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
+import AdminSearch from '../components/Admin/AdminSearch';
 
 import '../less/index.less';
 
@@ -15,6 +16,7 @@ const MainLayout = () => {
     <div>
       <Layout>
         <NavBar />
+        <AdminSearch />
         <Content className="padding-50 background-color-FFFFF6">
           <Breadcrumb className="margin-16-0">
             <Breadcrumb.Item>
@@ -22,9 +24,6 @@ const MainLayout = () => {
             </Breadcrumb.Item>
             <Breadcrumb.Item>
               <Link to="/calendar/">Calendar</Link>
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>
-              <Link to="/admin/">Admin</Link>
             </Breadcrumb.Item>
           </Breadcrumb>
           <div className="padding-24 min-height-380">
