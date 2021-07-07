557cb024a426c4d2cae8d3b433090fb063e3f02e
Danielle Koduru <69174710+DanielleKoduru@users.noreply.github.com>
2021-07-07 10:41:45 -0600

    moved button to event sidebar, created a location state, and  created a handle change for selecting the location

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -9,11 +9,11 @@ import parse from 'date-fns/parse';
 import startOfWeek from 'date-fns/startOfWeek';
 import getDay from 'date-fns/getDay';
 import Toolbar from './ResourcesToolbar';
-import { Menu, Dropdown } from 'antd';
 import ComputersList from './assign-computers/computers-list';
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 import CheckinModal from './CheckinModal';
 import EventListSideBar from './EventListSideBar';
+
 
 const locales = {
   'en-US': require('date-fns/locale/en-US'),
@@ -28,15 +28,6 @@ const localizer = dateFnsLocalizer({
 
 //drag and drop
 const TheCalendar = withDragAndDrop(Calendar);
-
-//location dropdown menu
-const schoolMenu = (
-  <Menu>
-    <Menu.Item key="1">India</Menu.Item>
-    <Menu.Item key="2">Africa</Menu.Item>
-    <Menu.Item key="3">All Locations</Menu.Item>
-  </Menu>
-);
 
 //custom toolbar and custom event displayed
 let components = {
@@ -80,6 +71,10 @@ const MyCalendar = (props) => {
   const showModal = () => {
     setIsModalVisible(true);
   };
+
+  // State to mange selecting locations
+  const [selectLocation, setSelectLocation] = useState(true)
+
 
   // Drag feature
   const handleDragStart = (event) => {
@@ -181,11 +176,6 @@ const MyCalendar = (props) => {
         setClickSelected={setClickSelected}
       />
       <div className="calendar-container">
-        <div className="rbc-toolbar rbc-btn-group">
-          <Dropdown overlay={schoolMenu}>
-            <button trigger={['click']}>Select Location</button>
-          </Dropdown>
-        </div>
         {/* if showCalendar is true, we give them the default, else we show the scheduler */}
         {showCalendar ? (
           <Calendar
--- src/components/Calendar/EventListSideBar.js
+++ src/components/Calendar/EventListSideBar.js
@@ -1,4 +1,6 @@
 import { events } from './data';
+import { Menu, Dropdown } from 'antd';
+
 
 import SideBarEvent from './SideBarEvent';
 
@@ -8,7 +10,7 @@ function EventListSideBar({ showModal, setClickSelected }) {
       if (
         e.eventStatus === false &&
         e.start.getMonth() + e.start.getDate() ===
-          new Date().getUTCMonth() + new Date().getUTCDate()
+        new Date().getUTCMonth() + new Date().getUTCDate()
       ) {
         {
           return e;
@@ -21,7 +23,7 @@ function EventListSideBar({ showModal, setClickSelected }) {
     if (
       e.eventStatus === true &&
       e.start.getMonth() + e.start.getDate() ===
-        new Date().getUTCMonth() + new Date().getUTCDate()
+      new Date().getUTCMonth() + new Date().getUTCDate()
     ) {
       {
         return e;
@@ -29,34 +31,54 @@ function EventListSideBar({ showModal, setClickSelected }) {
     }
   });
 
+  const schoolMenu = (
+    <Menu>
+      <Menu.Item key="1">India</Menu.Item>
+      <Menu.Item key="2">Africa</Menu.Item>
+      <Menu.Item key="3">All Locations</Menu.Item>
+    </Menu>
+  );
+
+// handle change for select location
+  const handleLocationChange = (props) => {
+    props.setSelectLocation({ name: events.target.value });
+  }
+
   return (
     <div className="sidebar-container">
-      <h4>Today's Sessions</h4>
-      <div className="events-container">
-        <div className="pendings">
-          <h5>Pending:</h5>
-          <div className="events">
-            {pendings.map((e) => (
-              <SideBarEvent
-                key={e.resourceId}
-                setClickSelected={setClickSelected}
-                showModal={showModal}
-                event={e}
-              />
-            ))}
-          </div>
+      <div className="calendar-container">
+        <div className="rbc-toolbar rbc-btn-group">
+          <Dropdown overlay={schoolMenu}>
+            <button trigger={['click']} onClick={handleLocationChange}>Select Location</button>
+          </Dropdown>
         </div>
-        <div className="checked-ins">
-          <h5>Checked-in:</h5>
-          <div className="events">
-            {checkedIns.map((e) => (
-              <SideBarEvent
-                key={e.resourceId}
-                setClickSelected={setClickSelected}
-                showModal={showModal}
-                event={e}
-              />
-            ))}
+        <h4>Today's Sessions</h4>
+        <div className="events-container">
+          <div className="pendings">
+            <h5>Pending:</h5>
+            <div className="events">
+              {pendings.map((e) => (
+                <SideBarEvent
+                  key={e.resourceId}
+                  setClickSelected={setClickSelected}
+                  showModal={showModal}
+                  event={e}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="checked-ins">
+            <h5>Checked-in:</h5>
+            <div className="events">
+              {checkedIns.map((e) => (
+                <SideBarEvent
+                  key={e.resourceId}
+                  setClickSelected={setClickSelected}
+                  showModal={showModal}
+                  event={e}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
