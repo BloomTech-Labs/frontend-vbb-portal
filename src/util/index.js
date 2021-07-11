a586130b4f058ede5d7c5db14178ec2ffb648500
patrickb42 <patrickbaldwin@gmail.com>
2021-07-11 19:56:56 -0400

    added meeting status to util export

--- src/util/index.js
+++ src/util/index.js
@@ -1,3 +1,6 @@
 export { formatPhoneNumber } from './formatPhoneNumber';
 export { getUserFromAuthToken } from './getUserWithAuthToken';
+export {
+  meetingStatus,
+} from './meetings';
 export { sleep } from './sleep';
