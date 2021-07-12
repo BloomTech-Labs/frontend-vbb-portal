00529e1b5d111f4170bc017e711ce053778de1c8
patrickb42 <patrickbaldwin@gmail.com>
2021-07-11 20:27:44 -0400

    EventListSideBar: forceRender feature, linting

--- src/components/Calendar/EventListSideBar.js
+++ src/components/Calendar/EventListSideBar.js
@@ -1,84 +1,47 @@
-import { events } from './data';
-import { Menu, Dropdown } from 'antd';
-
+import { useState } from 'react';
+import Moment from 'moment';
 
 import SideBarEvent from './SideBarEvent';
+import { events } from './data';
 
-function EventListSideBar({ showModal, setClickSelected, setSelectLocation }) {
-  const pendings = events
-    .filter((e) => {
-      if (
-        e.eventStatus === false &&
-        e.start.getMonth() + e.start.getDate() ===
-        new Date().getUTCMonth() + new Date().getUTCDate()
-      ) {
-        {
-          return e;
-        }
-      }
-    })
+function EventListSideBar({ showModal, setClickSelected }) {
+  const isScheduledToday = (e) => (Moment(e.start).isSame(Moment(Date.now()), 'day'));
+  // eslint-disable-next-line no-unused-vars
+  const [forceRenderHackVar, setForceRenderHackVar] = useState(false);
+  const forceRender = () => {
+    setForceRenderHackVar((oldValue) => !oldValue);
+  };
+  const todaysEvents = events.filter(isScheduledToday);
+
+  const pendingEvents = todaysEvents
+    .filter((e) => e.eventStatus === false)
     .sort((a, b) => new Date(a.start) - new Date(b.start));
+  const checkedInEvents = todaysEvents.filter((e) => e.eventStatus === true);
 
-  const checkedIns = events.filter((e) => {
-    if (
-      e.eventStatus === true &&
-      e.start.getMonth() + e.start.getDate() ===
-      new Date().getUTCMonth() + new Date().getUTCDate()
-    ) {
-      {
-        return e;
-      }
-    }
-  });
-
-  //change handler for select location
-  const handleLocationChange = (e) => {
-    setSelectLocation(e.item.props.value)
-  }
-
-  const schoolMenu = (
-    <Menu onClick={handleLocationChange}>
-      <Menu.Item value="India" key="1">India</Menu.Item>
-      <Menu.Item value="Africa" key="2">Africa</Menu.Item>
-      <Menu.Item value="AllLocations" key="3">All Locations</Menu.Item>
-    </Menu>
+  const buildSideBarEvent = (event) => (
+    <SideBarEvent
+      key={event.resourceId}
+      setClickSelected={setClickSelected}
+      showModal={showModal}
+      event={event}
+      forceRender={forceRender}
+    />
   );
 
   return (
     <div className="sidebar-container">
-      <div className="calendar-container">
-        <div className="rbc-toolbar rbc-btn-group">
-          <Dropdown overlay={schoolMenu}>
-            <button trigger={['click']}>Select Location</button>
-          </Dropdown>
-        </div>
-        <h4>Today's Sessions</h4>
-        <div className="events-container">
-          <div className="pendings">
-            <h5>Pending:</h5>
-            <div className="events">
-              {pendings.map((e) => (
-                <SideBarEvent
-                  key={e.resourceId}
-                  setClickSelected={setClickSelected}
-                  showModal={showModal}
-                  event={e}
-                />
-              ))}
-            </div>
+      <h4>Today&apos;s Sessions</h4>
+      <div className="events-container">
+        <div className="pendings">
+          <h5>Pending:</h5>
+          <div className="events">
+            {pendingEvents.map(buildSideBarEvent)}
           </div>
-          <div className="checked-ins">
-            <h5>Checked-in:</h5>
-            <div className="events">
-              {checkedIns.map((e) => (
-                <SideBarEvent
-                  key={e.resourceId}
-                  setClickSelected={setClickSelected}
-                  showModal={showModal}
-                  event={e}
-                />
-              ))}
-            </div>
+        </div>
+        <div className="checked-ins">
+          <h5>Checked-in:</h5>
+          <div className="events">
+            {checkedInEvents.map(buildSideBarEvent)}
           </div>
         </div>
       </div>
