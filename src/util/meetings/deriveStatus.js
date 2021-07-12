d4d2e43bf02838e1122ebf5ebaba4e00eb3ad8d9
patrickb42 <patrickbaldwin@gmail.com>
2021-07-11 20:01:07 -0400

    added deriveStatus's body

---
+++ src/util/meetings/deriveStatus.js
@@ -0,0 +1,24 @@
+import Moment from 'moment';
+
+import { meetingStatus } from '.';
+
+const {
+  CHECKED_IN,
+  EXPIRED,
+  NOT_CHECK_IN,
+  PRE_CHECK_IN,
+} = meetingStatus;
+
+export const deriveStatus = ({ start, end, checkedIn }) => {
+  const nowMoment = Moment(Date.now());
+  const halfHourBeforeMeetingStart = Moment(start).subtract(30, 'minutes');
+  const args = new Set([start, end, checkedIn]);
+
+  if (args.has('') || args.has(undefined)) return EXPIRED;
+  else if (checkedIn) return CHECKED_IN;
+  else if (nowMoment.isBefore(halfHourBeforeMeetingStart)) return PRE_CHECK_IN;
+  else if (nowMoment.isAfter(halfHourBeforeMeetingStart) && nowMoment.isBefore(end)) return NOT_CHECK_IN;
+  else return EXPIRED;
+};
+
+export default deriveStatus;
