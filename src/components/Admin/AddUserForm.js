f63a013e5465cf82293bff8f43778b7c0e271c50
Darren Tebo <darrentebo83@outlook.com>
2021-07-14 20:03:54 -0400

    Added role field to the user edit form (#122)
    * added and implemented useForm hook on the add/edit user form
    
    * fixed bug where form values weren't updating when using the useForm hook
    
    * added feature to display asterisk when a user has been edited, but not saved
    
    * added email field to the add/edit user form
    
    * added dob field to the add / edit user form
    
    * updated casing on variable names and moved props values to function arguments list instead of destructuring them
    
    * added clearer form save validation
    
    * updated message styling to make the message stand out more
    
    * added role field to the user edit form

--- src/components/Admin/AddUserForm.js
+++ src/components/Admin/AddUserForm.js
@@ -7,6 +7,7 @@ import {
   Form,
   Input,
   Switch,
+  Select,
   DatePicker,
   Space,
   Button
@@ -32,95 +33,132 @@ const switchLayout = {
 
 /*End Form Styling*/
 
+const { Option } = Select;
+
 const initialValues = {
   first_name: '',
   last_name: '',
   phone: '',
   email: '',
+  role: '',
 };
 
 const AddUserForm = ({ editMode, record, formUpdated }) => {
-  const [editedNotSaved, setEditedNotSaved] = useState(false);
-  const [values, handleChanges, clearForm] = useForm(initialValues);
+    const [editedNotSaved, setEditedNotSaved] = useState(false);
+    const [values, handleChanges, clearForm] = useForm(initialValues);
 
-  return (
-    <div>
-      <Form
-        {...layout}
-        name="basic"
-        className={editedNotSaved ? 'editing' : ''}
-        initialValues={initialValues}
-      >
-        {editMode ? (
-          <Form.Item {...switchLayout}>
-            <Switch
-              defaultChecked
-              checkedChildren="Activated"
-              unCheckedChildren="Deactivated"
-              name="activate"
-            />
-          </Form.Item>
-        ) : (
-          <></>
-        )}
-        <Form.Item label="First Name">
-          <Input
-            name="first_name"
-            defaultValue={editMode ? record.first_name : ''}
-            onChange={(e) => {
-              handleChanges(e);
-              setEditedNotSaved(true);
-            }}
-          />
-        </Form.Item>
-        <Form.Item label="Last Name">
-          <Input
-            name="last_name"
-            defaultValue={editMode ? record.last_name : ''}
-            onChange={(e) => {
-              handleChanges(e);
-              setEditedNotSaved(true);
-            }}
-          />
-        </Form.Item>
-        <Form.Item label="Phone">
-          <Input
-            name="phone"
-            defaultValue={editMode ? record.phone : ''}
-            onChange={(e) => {
-              handleChanges(e);
-              setEditedNotSaved(true);
-            }}
-          />
-        </Form.Item>
-        <Form.Item label="Email">
-          <Input
-            name="email"
-            defaultValue={editMode ? record.email : ''}
-            onChange={(e) => {
-              handleChanges(e);
-            }}
-          />
-        </Form.Item>
-        <Form.Item label="DOB">
-          <Space direction="vertical">
-            <DatePicker defaultValue={editMode ? record.dob : ''} />
-          </Space>
-        </Form.Item>
-        <Form.Item {...buttonLayout}>
-          <Button
-            className={editedNotSaved ? 'button-editing' : 'button'}
-            type="primary"
-            htmlType="submit"
-            onClick={() => setEditedNotSaved(false)}
-          >
-            {editMode ? 'Edit User' : 'Add New User'}
-          </Button>
-        </Form.Item>
-      </Form>
-      <Divider />
-    </div>
-  );
-};
+    return(
+        <div>
+            <Form
+                {...layout}
+                name= "basic"
+                className={editedNotSaved ? 'editing' : ''}
+                initialValues={initialValues}
+            >
+                {editMode ? (<Form.Item {...switchLayout}>
+                                <Switch
+                                    defaultChecked
+                                    checkedChildren="Activated"
+                                    unCheckedChildren="Deactivated"
+                                    name="activate"
+                                />
+                            </Form.Item>) : (
+                                <></>
+                            )}
+                <Form.Item
+                    label="First Name"
+                >
+                    <Input
+                        name="first_name" 
+                        defaultValue={editMode ? record.first_name : ''}
+                        onChange={
+                            (e) => {
+                                handleChanges(e);
+                                setEditedNotSaved(true);
+                            }
+                        }
+                    />
+                </Form.Item>
+                <Form.Item
+                    label="Last Name"
+                >
+                    <Input
+                        name="last_name"
+                        defaultValue={editMode ? record.last_name : ''}
+                        onChange={
+                            (e) => {
+                                handleChanges(e);
+                                setEditedNotSaved(true);
+                            }
+                        }
+                    />
+                </Form.Item>
+                <Form.Item
+                    label="Phone"
+                >
+                    <Input
+                        name="phone"
+                        defaultValue={editMode ? record.phone : ''}
+                        onChange={
+                            (e) => {
+                                handleChanges(e);
+                                setEditedNotSaved(true);
+                            }
+                        }
+                    />
+                </Form.Item>
+                <Form.Item
+                    label="Email"
+                >
+                    <Input
+                        name="email"
+                        defaultValue={editMode ? record.email : ''}
+                        onChange={
+                            (e) => {
+                                handleChanges(e);
+                            }
+                        }
+                    />
+                </Form.Item>
+                <Form.Item
+                    label="Role">
+                    <Select
+                        name="role"
+                        defaultValue={editMode ? record.role : ''}
+                        onChange={
+                            (e) => {
+                                handleChanges(e);
+                            }
+                        }
+                    >
+                        <Option value="Admin">Admin</Option>
+                        <Option value="Headmaster">Headmaster</Option>
+                        <Option value="Mentee">Mentee</Option>
+                        <Option value="Mentor">Mentor</Option>
+                    </Select>
+                </Form.Item>
+                <Form.Item
+                    label="DOB"
+                >
+                    <Space direction="vertical">
+                        <DatePicker
+                            defaultValue={editMode ? record.dob : ''}
+                        />
+                    </Space>
+                </Form.Item>
+                <Form.Item {...buttonLayout}>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        onClick={() => setEditedNotSaved(false)}
+                    >
+                        {editMode ? 'Edit User' : 'Add New User'}
+                    </Button>
+                </Form.Item>
+            </Form>
+            <Divider />
+        </div>
+    )
+}
 
 export default AddUserForm;
